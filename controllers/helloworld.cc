d8cd6085cb5630b5041878123810d3bbf1f675ca
tikikun <daogiatuank54@gmail.com>
2023-09-26 18:51:10 +0700

    done stream testing

--- controllers/helloworld.cc
+++ controllers/helloworld.cc
@@ -1,64 +1,121 @@
 #include "helloworld.h"
 #include "llama.h"
+#include <cstring>
 #include <drogon/HttpResponse.h>
-
 
 void helloworld::asyncHandleHttpRequest(
     const HttpRequestPtr &req,
     std::function<void(const HttpResponsePtr &)> &&callback) {
 
-    auto lock = llama.lock();
+  auto lock = llama.lock();
 
-    llama.rewind();
+  llama.rewind();
 
-    llama_reset_timings(llama.ctx);
+  llama_reset_timings(llama.ctx);
 
-    llama.prompt = "Hello there have a great day";
-    if (!llama.loadGrammar()) {
-      LOG_ERROR<< "Serious error happen here kekeke";
-      return;
-    }
+  llama.prompt = "Hello there have a great day";
+  if (!llama.loadGrammar()) {
+    LOG_ERROR << "Serious error happen here kekeke";
+    return;
+  }
 
-    llama.loadPrompt();
-    llama.beginCompletion();
+  llama.loadPrompt();
+  llama.beginCompletion();
 
-    size_t stop_pos = std::string::npos;
+  const auto chunked_content_provider =
+      [&](char *pBuffer, std::size_t nBuffSize) -> std::size_t {
+    size_t sent_count = 0;
+    size_t sent_token_probs_index = 0;
 
     while (llama.has_next_token) {
       const completion_token_output token_with_probs = llama.doCompletion();
+      if (token_with_probs.tok == -1 || llama.multibyte_pending > 0) {
+        continue;
+      }
       const std::string token_text =
-          token_with_probs.tok == -1
-              ? ""
-              : llama_token_to_piece(llama.ctx, token_with_probs.tok);
+          llama_token_to_piece(llama.ctx, token_with_probs.tok);
 
-      stop_pos = llama.findStoppingStrings(llama.generated_text,
-                                           token_text.size(), STOP_FULL);
-    }
+      size_t pos = std::min(sent_count, llama.generated_text.size());
 
-    if (stop_pos == std::string::npos) {
-      stop_pos =
-          llama.findStoppingStrings(llama.generated_text, 0, STOP_PARTIAL);
-    }
-    if (stop_pos != std::string::npos) {
-      llama.generated_text.erase(llama.generated_text.begin() + stop_pos,
-                                 llama.generated_text.end());
-    }
+      const std::string str_test = llama.generated_text.substr(pos);
+      bool is_stop_full = false;
+      size_t stop_pos =
+          llama.findStoppingStrings(str_test, token_text.size(), STOP_FULL);
+      if (stop_pos != std::string::npos) {
+        is_stop_full = true;
+        llama.generated_text.erase(llama.generated_text.begin() + pos +
+                                       stop_pos,
+                                   llama.generated_text.end());
+        pos = std::min(sent_count, llama.generated_text.size());
+      } else {
+        is_stop_full = false;
+        stop_pos = llama.findStoppingStrings(str_test, token_text.size(),
+                                             STOP_PARTIAL);
+      }
 
-    auto probs = llama.generated_token_probs;
-    if (llama.params.n_probs > 0 && llama.stopped_word) {
-      const std::vector<llama_token> stop_word_toks =
-          llama_tokenize(llama.ctx, llama.stopping_word, false);
-      probs = std::vector<completion_token_output>(
-          llama.generated_token_probs.begin(),
-          llama.generated_token_probs.end() - stop_word_toks.size());
-    }
+      if (stop_pos == std::string::npos ||
+          // Send rest of the text if we are at the end of the generation
+          (!llama.has_next_token && !is_stop_full && stop_pos > 0)) {
+        const std::string to_send =
+            llama.generated_text.substr(pos, std::string::npos);
 
-    const json data = format_final_response(llama, llama.generated_text, probs);
-    
-    auto response = drogon::HttpResponse::newHttpResponse();
-    response->setBody(data.dump());
+        sent_count += to_send.size();
+
+        std::vector<completion_token_output> probs_output = {};
+
+        if (llama.params.n_probs > 0) {
+          const std::vector<llama_token> to_send_toks =
+              llama_tokenize(llama.ctx, to_send, false);
+          size_t probs_pos = std::min(sent_token_probs_index,
+                                      llama.generated_token_probs.size());
+          size_t probs_stop_pos =
+              std::min(sent_token_probs_index + to_send_toks.size(),
+                       llama.generated_token_probs.size());
+          if (probs_pos < probs_stop_pos) {
+            probs_output = std::vector<completion_token_output>(
+                llama.generated_token_probs.begin() + probs_pos,
+                llama.generated_token_probs.begin() + probs_stop_pos);
+          }
+          sent_token_probs_index = probs_stop_pos;
+        }
+
+        const json data = format_partial_response(llama, to_send, probs_output);
+
+        const std::string str =
+            "data: " +
+            data.dump(-1, ' ', false, json::error_handler_t::replace) + "\n\n";
+
+        LOG_VERBOSE("data stream", {{"to_send", str}});
+        std::size_t nRead = std::min(str.size(), nBuffSize);
+        memcpy(pBuffer, str.data(), nRead);
+        return nRead;
+      }
+
+      if (!llama.has_next_token) {
+        // Generation is done, send extra information.
+        const json data = format_final_response(
+            llama, "",
+            std::vector<completion_token_output>(
+                llama.generated_token_probs.begin(),
+                llama.generated_token_probs.begin() + sent_token_probs_index));
+
+        const std::string str =
+            "data: " +
+            data.dump(-1, ' ', false, json::error_handler_t::replace) + "\n\n";
+
+        LOG_VERBOSE("data stream", {{"to_send", str}});
+        std::size_t nRead = std::min(str.size(), nBuffSize);
+        memcpy(pBuffer, str.data(), nRead);
+        return nRead;
+      }
+    }
     llama_print_timings(llama.ctx);
-  
-    
-  callback(response);
+    llama.mutex.unlock();
+    return 0;
+  };
+
+  auto resp = drogon::HttpResponse::newStreamResponse(chunked_content_provider,
+                                                      "chat_completions.txt");
+  callback(resp);
+  return;
 }
