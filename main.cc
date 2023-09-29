f1aee14159ac6f99e987d2f436062afc35e6e708
tikikun <daogiatuank54@gmail.com>
2023-09-29 15:17:31 +0700

    support mac and linux

--- main.cc
+++ main.cc
@@ -1,51 +1,49 @@
 
+#include <climits> // for PATH_MAX
 #include <drogon/drogon.h>
-#include <libgen.h>  // for dirname()
-#include <unistd.h>  // for readlink()
-#include <climits>   // for PATH_MAX
+#include <libgen.h> // for dirname()
+#include <unistd.h> // for readlink()
 
 #if defined(__APPLE__) && defined(__MACH__)
 #include <mach-o/dyld.h>
 #endif
 
-int main()
-{
-    char path[PATH_MAX];
-    std::string configPath;
+int main() {
+  char path[PATH_MAX];
+  std::string configPath;
 
 #if defined(__APPLE__) && defined(__MACH__)
-    uint32_t size = sizeof(path);
-    if (_NSGetExecutablePath(path, &size) == 0) {
-        path[size] = '\0';  // Null-terminate the string
-        char *dir = dirname(path);
-        configPath = std::string(dir) + "/config/config.json";
-    } else {
-        LOG_ERROR << "Failed to get binary location!";
-        return 1;
-    }
-#elif defined(__linux__)
-    ssize_t len = readlink("/proc/self/exe", path, sizeof(path)-1);
-    if (len != -1) {
-        path[len] = '\0';
-        char *dir = dirname(path);
-        configPath = std::string(dir) + "/config/config.json";
-    } else {
-        LOG_ERROR << "Failed to get binary location!";
-        return 1;
-    }
-#else
-    LOG_ERROR << "Unsupported platform!";
+  uint32_t size = sizeof(path);
+  if (_NSGetExecutablePath(path, &size) == 0) {
+    path[size] = '\0'; // Null-terminate the string
+    char *dir = dirname(path);
+    configPath = std::string(dir) + "/config/config.json";
+  } else {
+    LOG_ERROR << "Failed to get binary location!";
     return 1;
+  }
+#elif defined(__linux__)
+  ssize_t len = readlink("/proc/self/exe", path, sizeof(path) - 1);
+  if (len != -1) {
+    path[len] = '\0';
+    char *dir = dirname(path);
+    configPath = std::string(dir) + "/config/config.json";
+  } else {
+    LOG_ERROR << "Failed to get binary location!";
+    return 1;
+  }
+#else
+  LOG_ERROR << "Unsupported platform!";
+  return 1;
 #endif
 
-    // Set HTTP listener address and port
-    drogon::app().loadConfigFile(configPath);
-    auto app_conf = drogon::app().getCustomConfig();
+  // Set HTTP listener address and port
+  drogon::app().loadConfigFile(configPath);
+  auto app_conf = drogon::app().getCustomConfig();
 
-    LOG_INFO << app_conf["llama_model_file"].asString();
-    //drogon::app().addListener("0.0.0.0", 8080);
-    drogon::app().run();
+  LOG_INFO << app_conf["llama_model_file"].asString();
+  // drogon::app().addListener("0.0.0.0", 8080);
+  drogon::app().run();
 
-    return 0;
+  return 0;
 }
-
