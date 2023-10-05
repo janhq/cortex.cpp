13f9afeed86986e3a1f22501621825ee02170606
dotieuthien <dotieuthien9997@gmail.com>
2023-10-05 16:16:17 +0700

    Add path to binary file

--- main.cc
+++ main.cc
@@ -8,15 +8,17 @@
 #elif defined(__linux__)
 #include <libgen.h> // for dirname()
 #include <unistd.h> // for readlink()
+#elif defined(_WIN32)
+#include <windows.h>
 #else
 #error "Unsupported platform!"
 #endif
 
 int main() {
-  char path[PATH_MAX];
   std::string configPath;
 
 #if defined(__APPLE__) && defined(__MACH__)
+  char path[PATH_MAX];
   uint32_t size = sizeof(path);
   if (_NSGetExecutablePath(path, &size) == 0) {
     path[size] = '\0'; // Null-terminate the string
@@ -27,12 +29,30 @@ int main() {
     return 1;
   }
 #elif defined(__linux__)
+  char path[PATH_MAX];
   ssize_t len = readlink("/proc/self/exe", path, sizeof(path) - 1);
   if (len != -1) {
     path[len] = '\0';
     char *dir = dirname(path);
     configPath = std::string(dir) + "/config/config.json";
   } else {
+    LOG_ERROR << "Failed to get binary location!";
+    return 1;
+  }
+#elif defined(_WIN32)
+  char path[MAX_PATH];
+  char dir[MAX_PATH];
+  // char dir[MAX_PATH];
+  if(GetModuleFileNameA(NULL, path, sizeof(path))) {
+      char* lastBackslash = strrchr(path, '\\');
+      if (lastBackslash == nullptr) {
+        return 1;
+      }
+      lastBackslash[0] = '\0';
+      strcpy(dir, path);
+      configPath = std::string(dir) + "/config/config.json";
+  }
+  else {
     LOG_ERROR << "Failed to get binary location!";
     return 1;
   }
