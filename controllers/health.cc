a0694daa0ef11169aea7e350c4f1ad0c3e5fdd72
tikikun <daogiatuank54@gmail.com>
2023-10-05 15:11:00 +0700

    feat: add simple healthcheck

--- controllers/health.cc
+++ controllers/health.cc
@@ -3,13 +3,9 @@
 void health::asyncHandleHttpRequest(
     const HttpRequestPtr &req,
     std::function<void(const HttpResponsePtr &)> &&callback) {
-  // write your application logic here
   auto resp = HttpResponse::newHttpResponse();
-  // NOTE: The enum constant below is named "k200OK" (as in 200 OK), not
-  // "k2000K".
   resp->setStatusCode(k200OK);
   resp->setContentTypeCode(CT_TEXT_HTML);
   resp->setBody("Nitro is alive!!!");
   callback(resp);
-  // write your application logic here
 }
--- controllers/health.h
+++ controllers/health.h
@@ -10,7 +10,6 @@ class health : public drogon::HttpSimpleController<health>
   public:
     void asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback) override;
     PATH_LIST_BEGIN
-    // list path definitions here;
     PATH_ADD("/healthz", Get);
     PATH_LIST_END
 };
