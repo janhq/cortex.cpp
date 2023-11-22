05e72c0a428b26657afa0be9dbb60976563c4a7f
Faisal Amir <urmauur@gmail.com>
2023-11-22 14:46:38 +0700

    Fix content openai section

--- docs/src/containers/Homepage/OpenAI/index.js
+++ docs/src/containers/Homepage/OpenAI/index.js
@@ -53,47 +53,6 @@ export default function OpenAI() {
         </p>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <div className="relative">
-          <div className="mb-4">
-            <ThemedImage
-              alt="App screenshot"
-              sources={{
-                light: useBaseUrl("/img/logos/open-ai-dark.svg"),
-                dark: useBaseUrl("/img/logos/open-ai-light.svg"),
-              }}
-            />
-          </div>
-          <div className="group dark:bg-[#27272A]/50 bg-[#F4F4F5] border border-gray-300 dark:border-none py-2 px-4 rounded-md relative">
-            <div className="flex gap-x-2 items-center">
-              <p className="dark:text-yellow-400 text-yellow-600 font-medium">
-                POST
-              </p>
-              <p className="text-sm">
-                https://api.openai.com/v1/chat/completions
-              </p>
-            </div>
-            <div
-              className="group-hover:block hidden absolute bottom-2 right-2 text-xs px-2 py-1 rounded-md bg-gray-700 cursor-pointer text-white"
-              onClick={() =>
-                clipboard.copy("https://api.openai.com/v1/chat/completions")
-              }
-            >
-              {clipboard.copied ? "Copied" : "Copy"}
-            </div>
-          </div>
-          <div className="group [&>pre]:min-h-[400px]">
-            <SyntaxHighlighter language="bash" style={theme}>
-              {codeStringOpenAI}
-            </SyntaxHighlighter>
-            <div
-              className="group-hover:block hidden absolute top-24 right-2 text-xs px-2 py-1 rounded-md bg-gray-700 cursor-pointer text-white"
-              onClick={() => clipboard.copy(codeStringOpenAI)}
-            >
-              {clipboard.copied ? "Copied" : "Copy"}
-            </div>
-          </div>
-        </div>
-
         <div className="relative [&>pre]:min-h-[400px]">
           <div className="mb-2 flex items-center space-x-1">
             <img
@@ -130,6 +89,47 @@ export default function OpenAI() {
             <div
               className="group-hover:block hidden absolute top-24 right-2 text-xs px-2 py-1 rounded-md bg-gray-700 cursor-pointer text-white"
               onClick={() => clipboard.copy(codeStringNitro)}
+            >
+              {clipboard.copied ? "Copied" : "Copy"}
+            </div>
+          </div>
+        </div>
+
+        <div className="relative">
+          <div className="mb-4">
+            <ThemedImage
+              alt="App screenshot"
+              sources={{
+                light: useBaseUrl("/img/logos/open-ai-dark.svg"),
+                dark: useBaseUrl("/img/logos/open-ai-light.svg"),
+              }}
+            />
+          </div>
+          <div className="group dark:bg-[#27272A]/50 bg-[#F4F4F5] border border-gray-300 dark:border-none py-2 px-4 rounded-md relative">
+            <div className="flex gap-x-2 items-center">
+              <p className="dark:text-yellow-400 text-yellow-600 font-medium">
+                POST
+              </p>
+              <p className="text-sm">
+                https://api.openai.com/v1/chat/completions
+              </p>
+            </div>
+            <div
+              className="group-hover:block hidden absolute bottom-2 right-2 text-xs px-2 py-1 rounded-md bg-gray-700 cursor-pointer text-white"
+              onClick={() =>
+                clipboard.copy("https://api.openai.com/v1/chat/completions")
+              }
+            >
+              {clipboard.copied ? "Copied" : "Copy"}
+            </div>
+          </div>
+          <div className="group [&>pre]:min-h-[400px]">
+            <SyntaxHighlighter language="bash" style={theme}>
+              {codeStringOpenAI}
+            </SyntaxHighlighter>
+            <div
+              className="group-hover:block hidden absolute top-24 right-2 text-xs px-2 py-1 rounded-md bg-gray-700 cursor-pointer text-white"
+              onClick={() => clipboard.copy(codeStringOpenAI)}
             >
               {clipboard.copied ? "Copied" : "Copy"}
             </div>
