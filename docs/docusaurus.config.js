a665d3f6ba0ae93ede2855b8a2e69c9970b050f1
hahuyhoang411 <hahuyhoanghhh41@gmail.com>
2023-11-27 19:32:06 +0700

    draft SEO content

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -124,6 +124,16 @@ const config = {
       liveCodeBlock: {
         playgroundPosition: "bottom",
       },
+      metadata: [
+        { name: 'description', content: 'Nitro is a high-efficiency Large Language Model inference engine for edge computing.'},
+        { name: 'keywords', content: 'Nitro, OpenAI compatible, fast inference, local AI, llm, small AI, free, open source, production ready' },
+        { property: 'og:title', content: 'Embeddable AI | Nitro' },
+        { property: 'og:description', content: 'Nitro is a high-efficiency Large Language Model inference engine for edge computing.' },
+        { property: 'twitter:card', content: 'summary_large_image' },
+        { property: 'twitter:site', content: '@janhq_' }, 
+        { property: 'twitter:title', content: 'Embeddable AI | Nitro' },
+        { property: 'twitter:description', content: 'Nitro is a high-efficiency Large Language Model inference engine for edge computing.' },
+      ],
       navbar: {
         title: "Nitro",
         logo: {
