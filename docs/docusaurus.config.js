c7abe2e49a27a8494d429fa1f1feecb0acaa4336
hieu-jan <150573299+hieu-jan@users.noreply.github.com>
2023-12-08 04:34:29 +0900

    update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -126,7 +126,7 @@ const config = {
       metadata: [
         { name: 'description', content: 'Nitro is a high-efficiency Large Language Model inference engine for edge computing.'},
         { name: 'keywords', content: 'Nitro, Jan, fast inference, inference server, local AI, large language model, OpenAI compatible, open source, llama' },
-        { name: "robots", content: "all" },
+        { name: "robots", content: "index, follow" },
         { property: 'og:title', content: 'Fast inference engine | Nitro' },
         { property: 'og:description', content: 'Nitro is a high-efficiency Large Language Model inference engine for edge computing.' },
         { property: 'og:type', content: 'website'},
