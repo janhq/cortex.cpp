8515b97fa01d3db007500e39092602db971df2c6
Hien To <tominhhien97@gmail.com>
2024-01-03 21:17:09 +0700

    Add dockerfile for windows container runner

---
+++ .github/runners/runner.ps1
@@ -0,0 +1,2 @@
+.\actions-runner\config.cmd --unattended --replace --url https://github.com/${env:RUNNER_REPO} --token $env:RUNNER_TOKEN --runnergroup $env:RUNNER_GROUP --labels $env:RUNNER_LABEL --name $env:RUNNER_NAME --work $env:RUNNER_WORKDIR;
+.\actions-runner\run.cmd;
