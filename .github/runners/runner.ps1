d2cba72ccc98c7c07b235fe294009378985a60ab
Hien To <tominhhien97@gmail.com>
2024-01-04 13:39:25 +0700

    Add Dockerfile for linux github runner agent

--- .github/runners/runner.ps1
+++ .github/runners/runner.ps1
@@ -1,2 +1,2 @@
-.\actions-runner\config.cmd --unattended --replace --url https://github.com/${env:RUNNER_REPO} --token $env:RUNNER_TOKEN --runnergroup $env:RUNNER_GROUP --labels $env:RUNNER_LABEL --name $env:RUNNER_NAME --work $env:RUNNER_WORKDIR;
+.\actions-runner\config.cmd --unattended --replace --url https://github.com/${env:RUNNER_REPO} --pat $env:RUNNER_PAT --runnergroup $env:RUNNER_GROUP --labels $env:RUNNER_LABELS --name $env:RUNNER_NAME --work $env:RUNNER_WORKDIR;
 .\actions-runner\run.cmd;
--- .github/runners/start.sh
+++ .github/runners/start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 RUNNER_REPO=$RUNNER_REPO
-RUNNER_TOKEN=$RUNNER_TOKEN
+RUNNER_PAT=$RUNNER_PAT
 RUNNER_GROUP=$RUNNER_GROUP
 RUNNER_LABELS=$RUNNER_LABELS
 RUNNER_NAME=$RUNNER_NAME
 
 cd /home/runner/actions-runner
 
-./config.sh --unattended --url https://github.com/${RUNNER_REPO} --token ${RUNNER_TOKEN} --name ${RUNNER_NAME} --runnergroup ${RUNNER_GROUP} --labels ${RUNNER_LABELS} --work /home/runner/actions-runner/_work
+./config.sh --unattended --replace --url https://github.com/${RUNNER_REPO} --pat ${RUNNER_PAT} --name ${RUNNER_NAME} --runnergroup ${RUNNER_GROUP} --labels ${RUNNER_LABELS} --work /home/runner/actions-runner/_work
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${RUNNER_TOKEN}
+    ./config.sh remove --unattended --pat ${RUNNER_PAT}
 }
 
 trap 'cleanup; exit 130' INT
