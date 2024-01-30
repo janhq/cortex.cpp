506d2778f0d01b2754d81661311f0c9844f839a8
InNoobWeTrust <vu.truong.16193@gmail.com>
2024-01-30 15:24:05 +0700

    feat(nitr-node/postinstall): download latest release for nitro by default
    Can also override the version by setting NITRO_VERSION environment variable

--- nitro-node/src/scripts/download-nitro.ts
+++ nitro-node/src/scripts/download-nitro.ts
@@ -3,12 +3,171 @@ import path from "node:path";
 import download from "download";
 import { Duplex } from "node:stream";
 
-// Define nitro version to download in env variable
-const NITRO_VERSION = process.env.NITRO_VERSION || "0.2.11";
+// Define nitro version to download in env variable ("latest" or tag "v1.2.3")
+const NITRO_VERSION = process.env.NITRO_VERSION || "latest";
 // The platform OS to download nitro for
 const PLATFORM = process.env.npm_config_platform || process.platform;
 // The platform architecture
 //const ARCH = process.env.npm_config_arch || process.arch;
+
+const releaseUrlPrefixVariants = {
+  latest: "https://api.github.com/repos/janhq/nitro/releases/",
+  tag: "https://api.github.com/repos/janhq/nitro/releases/tags/",
+};
+
+const getReleaseInfo = async (taggedVersion: string): Promise<any> => {
+  const releaseUrlPrefix =
+    taggedVersion === "latest"
+      ? releaseUrlPrefixVariants.latest
+      : taggedVersion.match(/^\d+\.\d+\.\d+$/gi)
+        ? releaseUrlPrefixVariants.tag
+        : undefined;
+  if (!releaseUrlPrefix) throw Error(`Invalid version: ${taggedVersion}`);
+  const url = `${releaseUrlPrefix}${taggedVersion}`;
+  console.log(`[Getting release info] ${url}`);
+  const response = await fetch(url, {
+    headers: {
+      Accept: "application/vnd.github+json",
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+  if (!response.ok)
+    throw Error(`Failed to fetch release info: ${response.status}`);
+  return await response.json();
+};
+
+const extractDownloadInfo = async (
+  /* releaseInfo */
+  {
+    html_url,
+    tag_name,
+    name,
+    prerelease,
+    assets,
+  }: {
+    html_url: string;
+    tag_name: string;
+    name: string;
+    prerelease: boolean;
+    assets: {
+      name: string;
+      size: number;
+      browser_download_url: string;
+    }[];
+  },
+  /* variants for filter for in format {suffix: dir} */
+  suffixVariants: Record<string, string>,
+): Promise<Record<string, { url: string; dir: string }>> => {
+  console.log(`[Release URL][prerelease=${prerelease}] ${html_url}`);
+  const assetNames = assets.map(({ name }: { name: string }) => name);
+  const assetsToDownloads = Object.entries(suffixVariants).reduce(
+    (
+      dict: Record<string, { url: string; dir: string }>,
+      [suffix, dir]: [string, string],
+    ) => {
+      // Skip if suffix is not in asset names
+      if (!assetNames.includes(`nitro-${name}-${suffix}.tar.gz`)) return dict;
+      // Else add the download url
+      dict[suffix] = {
+        url: `https://github.com/janhq/nitro/releases/download/${tag_name}/nitro-${name}-${suffix}.tar.gz`,
+        dir,
+      };
+      return dict;
+    },
+    {},
+  );
+  // If empty then no assets were found
+  if (!Object.keys(assetsToDownloads).length)
+    throw Error("Failed to find any asset to download");
+  // Return the dict of download info
+  return assetsToDownloads;
+};
+
+// Report download progress
+const createProgressReporter =
+  (variant: string) => (stream: Promise<Buffer> & Duplex) =>
+    stream
+      .on(
+        "downloadProgress",
+        (progress: { transferred: any; total: any; percent: number }) => {
+          // Print and update progress on a single line of terminal
+          process.stdout.write(
+            `\r\x1b[K[${variant}] ${progress.transferred}/${progress.total} ${Math.floor(progress.percent * 100)}%...`,
+          );
+        },
+      )
+      .on("end", () => {
+        // Jump to new line to log next message
+        console.log();
+        console.log(`[${variant}] Finished downloading!`);
+      });
+
+// Download single binary
+const downloadBinary = async (
+  suffix: string,
+  { url, dir }: { url: string; dir: string },
+) => {
+  console.log(`Downloading ${url} to ${dir}...`);
+  const progressReporter = createProgressReporter(suffix);
+  await progressReporter(
+    download(url, dir, {
+      strip: 1,
+      extract: true,
+    }),
+  );
+  // Set mode of downloaded binaries to executable
+  (fs.readdirSync(dir, { recursive: true }) as string[])
+    .filter(
+      (fname) =>
+        fname.includes("nitro") && fs.lstatSync(path.join(dir, fname)).isFile(),
+    )
+    .forEach((nitroBinary) => {
+      const absPath = path.join(dir, nitroBinary);
+      // Set mode executable for nitro binary
+      fs.chmodSync(
+        absPath,
+        fs.constants.S_IRWXU | fs.constants.S_IRWXG | fs.constants.S_IRWXO,
+      );
+    });
+};
+
+// Download the binaries
+const downloadBinaries = (
+  /* downloadInfo */
+  downloadInfo: Record<string, { url: string; dir: string }>,
+  /* The absolute path to the directory where binaries will be downloaded */
+  absBinDirPath: string,
+) => {
+  return Object.entries(downloadInfo).reduce(
+    (
+      p: Promise<any>,
+      [suffix, { url, dir }]: [string, { url: string; dir: string }],
+    ) =>
+      p.then(() =>
+        downloadBinary(suffix, { url, dir: path.join(absBinDirPath, dir) }),
+      ),
+    Promise.resolve(),
+  );
+};
+
+// Check for a files with nitro in name in the corresponding directory
+const verifyDownloadedBinaries = (absBinDirPath: string) => {
+  // Check all the paths in variantConfig for a file with nitro in its name
+  return Object.values(variantConfig).every((binDirVariant: string) => {
+    try {
+      const dirToCheck = path.join(absBinDirPath, binDirVariant);
+      const pathToCheck = path.join(dirToCheck, "nitro");
+      return (fs.readdirSync(dirToCheck, { recursive: true }) as string[]).some(
+        (fname) => {
+          const fullPath = path.join(dirToCheck, fname);
+          return fullPath.startsWith(pathToCheck);
+        },
+      );
+    } catch (_e: any) {
+      return false;
+    }
+  });
+};
 
 const linuxVariants = {
   "linux-amd64": "linux-cpu",
@@ -37,95 +196,9 @@ const variantMapping: Record<string, Record<string, string>> = {
 if (!(PLATFORM in variantMapping)) {
   throw Error(`Invalid platform: ${PLATFORM}`);
 }
+
 // Get download config for this platform
 const variantConfig: Record<string, string> = variantMapping[PLATFORM];
-
-// Generate download link for each tarball
-const getTarUrl = (version: string, suffix: string) =>
-  `https://github.com/janhq/nitro/releases/download/v${version}/nitro-${version}-${suffix}.tar.gz`;
-
-// Report download progress
-const createProgressReporter =
-  (variant: string) => (stream: Promise<Buffer> & Duplex) =>
-    stream
-      .on(
-        "downloadProgress",
-        (progress: { transferred: any; total: any; percent: number }) => {
-          // Print and update progress on a single line of terminal
-          process.stdout.write(
-            `\r\x1b[K[${variant}] ${progress.transferred}/${progress.total} ${Math.floor(progress.percent * 100)}%...`,
-          );
-        },
-      )
-      .on("end", () => {
-        // Jump to new line to log next message
-        console.log();
-        console.log(`[${variant}] Finished downloading!`);
-      });
-
-// Download single binary
-const downloadBinary = async (
-  version: string,
-  suffix: string,
-  destDirPath: string,
-) => {
-  const tarUrl = getTarUrl(version, suffix);
-  console.log(`Downloading ${tarUrl} to ${destDirPath}`);
-  const progressReporter = createProgressReporter(suffix);
-  await progressReporter(
-    download(tarUrl, destDirPath, {
-      strip: 1,
-      extract: true,
-    }),
-  );
-  // Set mode of downloaded binaries to executable
-  (fs.readdirSync(destDirPath, { recursive: true }) as string[])
-    .filter(
-      (fname) =>
-        fname.includes("nitro") &&
-        fs.lstatSync(path.join(destDirPath, fname)).isFile(),
-    )
-    .forEach((nitroBinary) => {
-      const absPath = path.join(destDirPath, nitroBinary);
-      fs.chmodSync(
-        absPath,
-        fs.constants.S_IRWXU | fs.constants.S_IRWXG | fs.constants.S_IRWXO,
-      );
-    });
-};
-
-// Download the binaries
-const downloadBinaries = (
-  version: string,
-  config: Record<string, string>,
-  absBinDirPath: string,
-) => {
-  return Object.entries(config).reduce(
-    (p: Promise<any>, [k, v]) =>
-      p.then(() => downloadBinary(version, k, path.join(absBinDirPath, v))),
-    Promise.resolve(),
-  );
-};
-
-// Check for a files with nitro in name in the corresponding directory
-const verifyDownloadedBinaries = (absBinDirPath: string) => {
-  // Check all the paths in variantConfig for a file with nitro in its name
-  return Object.values(variantConfig).every((binDirVariant: string) => {
-    try {
-      const dirToCheck = path.join(absBinDirPath, binDirVariant);
-      const pathToCheck = path.join(dirToCheck, "nitro");
-      return (fs.readdirSync(dirToCheck, { recursive: true }) as string[]).some(
-        (fname) => {
-          const fullPath = path.join(dirToCheck, fname);
-          return fullPath.startsWith(pathToCheck);
-        },
-      );
-    } catch (_e: any) {
-      return false;
-    }
-  });
-};
-
 // Call the download function with version and config
 export const downloadNitro = async (absBinDirPath: string) => {
   // Return early without downloading if nitro binaries are already downloaded
@@ -133,12 +206,14 @@ export const downloadNitro = async (absBinDirPath: string) => {
     //console.log("Nitro binaries are already downloaded!");
     return;
   }
-  return await downloadBinaries(NITRO_VERSION, variantConfig, absBinDirPath);
+  const releaseInfo = await getReleaseInfo(NITRO_VERSION);
+  const downloadInfo = await extractDownloadInfo(releaseInfo, variantConfig);
+  return await downloadBinaries(downloadInfo, absBinDirPath);
 };
 
 // Run script if called directly instead of import as module
 if (require.main === module) {
   // Assume calling the source typescript files
   // bin path will be relative to the source code root
-  downloadNitro(path.join(__dirname, '..', "..", "bin"));
+  downloadNitro(path.join(__dirname, "..", "..", "bin"));
 }
