51c333aa251a1bff19137a2ec2c6d39efc34e7a9
hiro <vuonghoainam.work@gmail.com>
2024-01-31 22:07:35 +0700

    fix(ci): Add e2e test for windows

--- .github/scripts/e2e-test-llama-windows.bat
+++ .github/scripts/e2e-test-llama-windows.bat
@@ -66,7 +66,7 @@ echo curl_data2=%curl_data2%
 rem Run the curl commands and capture the status code
 curl.exe -o "%TEMP%\response1.log" -s -w "%%{http_code}" --location "http://127.0.0.1:%PORT%/inferences/llamacpp/loadModel" --header "Content-Type: application/json" --data "%curl_data1%" > %TEMP%\response1.log 2>&1
 
-curl.exe -o "%TEMP%\response2.log" -s -w "%%{http_code}" --location "http://127.0.0.1:%PORT%/inferences/llamacpp/chat_completion" ^
+curl.exe -o "%TEMP%\response2.log" -s -w "%%{http_code}" --location "http://127.0.0.1:%PORT%/v1/chat/completions" ^
 --header "Content-Type: application/json" ^
 --header "Accept: text/event-stream" ^
 --header "Access-Control-Allow-Origin: *" ^
@@ -78,13 +78,13 @@ rem Read the status codes from the log files
 for /f %%a in (%TEMP%\response1.log) do set "response1=%%a"
 for /f %%a in (%TEMP%\response2.log) do set "response2=%%a"
 
-if "%response1%" neq "000" (
+if "%response1%" neq "200" (
     echo The first curl command failed with status code: %response1%
     type %TEMP%\response1.log
     set "error_occurred=1"
 )
 
-if "%response2%" neq "000" (
+if "%response2%" neq "200" (
     echo The second curl command failed with status code: %response2%
     type %TEMP%\response2.log
     set "error_occurred=1"
