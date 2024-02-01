5eac107b3c59b728f9b88fcc1c419355074f84a1
hiro <vuonghoainam.work@gmail.com>
2024-02-01 16:51:15 +0700

    feat: add try catch on drogon for binding port and host

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@
 #include <drogon/HttpAppFramework.h>
 #include <drogon/drogon.h>
 #include <iostream>
+#include <stdexcept> // for std::runtime_error
 
 #if defined(__APPLE__) && defined(__MACH__)
 #include <libgen.h> // for dirname()
@@ -23,22 +24,16 @@ int main(int argc, char *argv[]) {
   int port = 3928;
   std::string uploads_folder_path;
 
-  // Number of nitro threads
+  // Parse command-line arguments
   if (argc > 1) {
     thread_num = std::atoi(argv[1]);
   }
-
-  // Check for host argument
   if (argc > 2) {
     host = argv[2];
   }
-
-  // Check for port argument
   if (argc > 3) {
-    port = std::atoi(argv[3]); // Convert string argument to int
+    port = std::atoi(argv[3]);
   }
-
-  // Uploads folder path
   if (argc > 4) {
     uploads_folder_path = argv[4];
   }
@@ -46,20 +41,30 @@ int main(int argc, char *argv[]) {
   int logical_cores = std::thread::hardware_concurrency();
   int drogon_thread_num = std::max(thread_num, logical_cores);
   nitro_utils::nitro_logo();
+
 #ifdef NITRO_VERSION
   LOG_INFO << "Nitro version: " << NITRO_VERSION;
 #else
   LOG_INFO << "Nitro version: undefined";
 #endif
-  LOG_INFO << "Server started, listening at: " << host << ":" << port;
-  LOG_INFO << "Please load your model";
-  drogon::app().addListener(host, port);
+
+  // Attempt to add listener and catch exception if port is already in use
+  try {
+    drogon::app().addListener(host, port);
+  } catch (const std::exception &e) {
+    std::cerr << "Failed to bind to port " << port << ": " << e.what()
+              << std::endl;
+    return 1; // Exit with error code
+  }
+
   drogon::app().setThreadNum(drogon_thread_num);
   if (!uploads_folder_path.empty()) {
     LOG_INFO << "Drogon uploads folder is at: " << uploads_folder_path;
     drogon::app().setUploadPath(uploads_folder_path);
   }
-  LOG_INFO << "Number of thread is:" << drogon::app().getThreadNum();
+  LOG_INFO << "Number of threads is: " << drogon::app().getThreadNum();
+  LOG_INFO << "Server started, listening at: " << host << ":" << port;
+  LOG_INFO << "Please load your model";
 
   drogon::app().run();
 
