86307796228d61fad17e43a6102533699a6251a1
tikikun <daogiatuank54@gmail.com>
2024-02-06 16:04:27 +0700

    add base class for inference provider

---
+++ common/base.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <drogon/HttpController.h>
+
+using namespace drogon;
+
+#pragma once
+#include <drogon/HttpController.h>
+
+using namespace drogon;
+
+class BaseProvider {
+public:
+  virtual ~BaseProvider() {}
+
+  // General inference method
+  virtual void
+  inference(const HttpRequestPtr &req,
+            std::function<void(const HttpResponsePtr &)> &&callback) = 0;
+
+  // Model management
+  virtual void
+  loadModel(const HttpRequestPtr &req,
+            std::function<void(const HttpResponsePtr &)> &&callback) = 0;
+  virtual void
+  unloadModel(const HttpRequestPtr &req,
+              std::function<void(const HttpResponsePtr &)> &&callback) = 0;
+  virtual void
+  modelStatus(const HttpRequestPtr &req,
+              std::function<void(const HttpResponsePtr &)> &&callback) = 0;
+
+  // Additional methods
+  void handlePrelight(const HttpRequestPtr &req,
+                      std::function<void(const HttpResponsePtr &)> &&callback);
+};
+
+class ChatProvider : public BaseProvider {
+public:
+  virtual ~ChatProvider() {}
+
+  // Implement embedding functionality specific to chat
+  virtual void
+  embedding(const HttpRequestPtr &req,
+            std::function<void(const HttpResponsePtr &)> &&callback) = 0;
+
+  // The derived class can also override other methods if needed
+};
