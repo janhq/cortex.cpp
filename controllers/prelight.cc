0a9a1bdd66374d51067e11d0f2ba8b8ff7129833
tikikun <daogiatuank54@gmail.com>
2024-02-16 16:16:08 +0700

    feat: add prelight method

--- controllers/prelight.cc
+++ controllers/prelight.cc
@@ -1,3 +1,13 @@
 #include "prelight.h"
 
-// Add definition of your processing function here
+void prelight::handlePrelight(
+    const HttpRequestPtr &req,
+    std::function<void(const HttpResponsePtr &)> &&callback) {
+  auto resp = drogon::HttpResponse::newHttpResponse();
+  resp->setStatusCode(drogon::HttpStatusCode::k200OK);
+  resp->addHeader("Access-Control-Allow-Origin", "*");
+  resp->addHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  resp->addHeader("Access-Control-Allow-Headers", "*");
+  callback(resp);
+}
+
