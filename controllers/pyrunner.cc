99ea995a61c880cfb78f2b57917f4d88d7db1126
tikikun <daogiatuank54@gmail.com>
2024-04-10 22:53:43 +0700

    feat: example testrun for python runtime in nitro

--- controllers/pyrunner.cc
+++ controllers/pyrunner.cc
@@ -1,3 +1,43 @@
 #include "pyrunner.h"
+#include <dlfcn.h>
+#include "utils/nitro_utils.h"
 
+void pyrunner::testrun(const HttpRequestPtr& req,
+                       std::function<void(const HttpResponsePtr&)>&& callback) {
+
+  printf("Starting program...\n");
+
+  void* libPython = dlopen(default_python_lib_dir.c_str(), RTLD_LAZY);
+  if (!libPython) {
+    fprintf(stderr, "Failed to load Python library: %s\n", dlerror());
+  }
+
+  auto Py_Initialize = (Py_InitializeFunc)dlsym(libPython, "Py_Initialize");
+  auto Py_Finalize = (Py_FinalizeFunc)dlsym(libPython, "Py_Finalize");
+  auto PyErr_Print = (PyErr_PrintFunc)dlsym(libPython, "PyErr_Print");
+
+  auto PyRun_SimpleString =
+      (PyRun_SimpleStringFunc)dlsym(libPython, "PyRun_SimpleString");
+  auto PyRun_SimpleFile =
+      (PyRun_SimpleFileFunc)dlsym(libPython, "PyRun_SimpleFile");
+
+  if (!Py_Initialize || !Py_Finalize || !PyRun_SimpleString ||
+      !PyRun_SimpleFile) {
+    fprintf(stderr, "Failed to bind necessary Python functions\n");
+    dlclose(libPython);
+  }
+
+  Py_Initialize();
+
+  PyRun_SimpleString("print('hello world')");
+
+  Py_Finalize();
+  dlclose(libPython);
+
+  Json::Value jsonResp;
+  jsonResp["message"] = "Python test run succesfully done";
+  auto response = nitro_utils::nitroHttpJsonResponse(jsonResp);
+  callback(response);
+  return;
+};
 // Add definition of your processing function here
--- controllers/pyrunner.h
+++ controllers/pyrunner.h
@@ -1,20 +1,59 @@
 #pragma once
 
 #include <drogon/HttpController.h>
+#include <drogon/HttpTypes.h>
+#include <filesystem>
+#include <regex>
+#include <string>
 
 using namespace drogon;
+namespace fs = std::filesystem;
 
-class pyrunner : public drogon::HttpController<pyrunner>
-{
-  public:
-    METHOD_LIST_BEGIN
-    // use METHOD_ADD to add your custom processing function here;
-    // METHOD_ADD(pyrunner::get, "/{2}/{1}", Get); // path is /pyrunner/{arg2}/{arg1}
-    // METHOD_ADD(pyrunner::your_method_name, "/{1}/{2}/list", Get); // path is /pyrunner/{arg1}/{arg2}/list
-    // ADD_METHOD_TO(pyrunner::your_method_name, "/absolute/path/{1}/{2}/list", Get); // path is /absolute/path/{arg1}/{arg2}/list
+typedef void (*Py_InitializeFunc)();
+typedef void (*Py_FinalizeFunc)();
+typedef void (*PyErr_PrintFunc)();
+typedef int (*PyRun_SimpleStringFunc)(const char*);
+typedef int (*PyRun_SimpleFileFunc)(FILE*, const char*);
 
-    METHOD_LIST_END
-    // your declaration of processing function maybe like this:
-    // void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, int p1, std::string p2);
-    // void your_method_name(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, double p1, int p2) const;
+class pyrunner : public drogon::HttpController<pyrunner> {
+ public:
+  pyrunner() {
+    LOG_INFO << "Looking for libpython inside lib";
+    default_python_lib_dir = findPythonLib("lib");
+    LOG_INFO << "Found Python in path: " + default_python_lib_dir;
+  }
+  METHOD_LIST_BEGIN
+
+  ADD_METHOD_TO(pyrunner::testrun, "/testrun", Get);
+
+  // Method declarations...
+  METHOD_LIST_END
+
+ private:
+  std::string default_python_lib_dir;
+
+  void testrun(const HttpRequestPtr& req,
+               std::function<void(const HttpResponsePtr&)>&& callback);
+
+  std::string findPythonLib(const std::string& libDir) {
+    std::string pattern;
+#if defined(_WIN32) || defined(_WIN64)
+    // Windows
+    pattern = "libpython[0-9]+\\.[0-9]+\\.dll";
+#elif defined(__APPLE__) || defined(__MACH__)
+    // macOS
+    pattern = "libpython[0-9]+\\.[0-9]+\\.dylib";
+#else
+    // Linux or other Unix-like systems
+    pattern = "libpython[0-9]+\\.[0-9]+\\.so";
+#endif
+    std::regex regexPattern(pattern);
+    for (const auto& entry : fs::directory_iterator(libDir)) {
+      std::string fileName = entry.path().filename().string();
+      if (std::regex_match(fileName, regexPattern)) {
+        return entry.path().string();
+      }
+    }
+    return "";  // Return an empty string if no matching library is found
+  }
 };
