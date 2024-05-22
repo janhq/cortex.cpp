1cd35d1ca2898938eb35c7df63873c78f2ad8453
vansangpfiev <vansangpfiev@gmail.com>
2024-05-22 16:57:41 +0700

    test: linux

--- .github/scripts/e2e-test-python-linux-and-mac.sh
+++ .github/scripts/e2e-test-python-linux-and-mac.sh
@@ -23,6 +23,7 @@ PORT=$((RANDOM % range + min))
 # Install numpy for Python
 export PYTHONHOME=$(pwd)/engines/cortex.python/python/
 export LD_LIBRARY_PATH="$PYTHONHOME:$LD_LIBRARY_PATH"
+export DYLD_FALLBACK_LIBRARY_PATH="$PYTHONHOME:$DYLD_FALLBACK_LIBRARY_PATH"
 echo "Set Python HOME to $PYTHONHOME"
 echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
 ./engines/cortex.python/python/bin/python3 -m ensurepip
