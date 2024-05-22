c8e3e84d851042b273786aace06c757627ea0823
James <namnh0122@gmail.com>
2024-05-23 02:03:10 +0700

    feat: store messages when chat via cli
    Signed-off-by: James <namnh0122@gmail.com>

--- cortex-js/src/main.ts
+++ cortex-js/src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { defaultCortexJsHost, defaultCortexJsPort } from 'constant';
+import { SeedService } from './usecases/seed/seed.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     snapshot: true,
     cors: true,
   });
+  const seedService = app.get(SeedService);
+  await seedService.seed();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,14 +19,7 @@ async function bootstrap() {
     }),
   );
 
-  const config = new DocumentBuilder()
-    .setTitle('Cortex API')
-    .setDescription('The Cortex API description')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('api', app, document);
+  buildSwagger(app);
 
   // getting port from env
   const host = process.env.CORTEX_JS_HOST || defaultCortexJsHost;
@@ -32,5 +28,16 @@ async function bootstrap() {
   await app.listen(port, host);
   console.log(`Server running on ${host}:${port}`);
 }
+
+const buildSwagger = (app: INestApplication<any>) => {
+  const config = new DocumentBuilder()
+    .setTitle('Cortex API')
+    .setDescription('The Cortex API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+};
 
 bootstrap();
