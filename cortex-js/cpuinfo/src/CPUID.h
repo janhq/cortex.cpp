bd4d5de14e2a360e39697e28de7589fdafe1fddf
hiento09 <136591877+hiento09@users.noreply.github.com>
2024-05-24 16:23:30 +0700

    Add cpuinfo to help check cpu instruction (#611)
    Co-authored-by: Hien To <tominhhien97@gmail.com>

---
+++ cortex-js/cpuinfo/src/CPUID.h
@@ -0,0 +1,33 @@
+#ifndef CPUID_H
+#define CPUID_H
+
+#ifdef _WIN32
+#include <limits.h>
+#include <intrin.h>
+typedef unsigned __int32  uint32_t;
+#else
+#include <stdint.h>
+#endif
+
+class CPUID {
+  uint32_t regs[4];
+
+public:
+  explicit CPUID(unsigned i) {
+#ifdef _WIN32
+    __cpuid((int *)regs, (int)i);
+#else
+    asm volatile
+      ("cpuid" : "=a" (regs[0]), "=b" (regs[1]), "=c" (regs[2]), "=d" (regs[3])
+       : "a" (i), "c" (0));
+    // ECX is set to zero for CPUID function 4
+#endif
+  }
+
+  const uint32_t &EAX() const {return regs[0];}
+  const uint32_t &EBX() const {return regs[1];}
+  const uint32_t &ECX() const {return regs[2];}
+  const uint32_t &EDX() const {return regs[3];}
+};
+
+#endif // CPUID_H
