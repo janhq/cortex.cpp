68f84fbbf40b89bcb0a3452f21095ad1f9f7faab
Louis <louis@jan.ai>
2024-05-29 10:29:00 +0700

    chore: cortex chat revamp with options (#620)

--- cortex-js/src/infrastructure/controllers/threads.controller.ts
+++ cortex-js/src/infrastructure/controllers/threads.controller.ts
@@ -6,7 +6,6 @@ import {
   Patch,
   Param,
   Delete,
-  HttpCode
   UseInterceptors,
 } from '@nestjs/common';
 import { ThreadsUsecases } from '@/usecases/threads/threads.usecases';
@@ -14,14 +13,7 @@ import { CreateThreadDto } from '@/infrastructure/dtos/threads/create-thread.dto
 import { UpdateThreadDto } from '@/infrastructure/dtos/threads/update-thread.dto';
 import { DeleteThreadResponseDto } from '@/infrastructure/dtos/threads/delete-thread.dto';
 import { GetThreadResponseDto } from '@/infrastructure/dtos/threads/get-thread.dto';
-import {
-  ApiCreatedResponse,
-  ApiOkResponse,
-  ApiOperation,
-  ApiParam,
-  ApiTags,
-  ApiResponse
-} from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiTags, ApiResponse } from '@nestjs/swagger';
 import { TransformInterceptor } from '../interceptors/transform.interceptor';
 
 @ApiTags('Threads')
@@ -30,13 +22,20 @@ import { TransformInterceptor } from '../interceptors/transform.interceptor';
 export class ThreadsController {
   constructor(private readonly threadsService: ThreadsUsecases) {}
 
-  @ApiOperation({ summary: 'Create thread', description: "Creates a new thread." })
+  @ApiOperation({
+    summary: 'Create thread',
+    description: 'Creates a new thread.',
+  })
   @Post()
   create(@Body() createThreadDto: CreateThreadDto) {
     return this.threadsService.create(createThreadDto);
   }
 
-  @ApiOperation({ summary: 'List threads', description: "Lists all the available threads along with its configurations." })
+  @ApiOperation({
+    summary: 'List threads',
+    description:
+      'Lists all the available threads along with its configurations.',
+  })
   @Get()
   findAll() {
     return this.threadsService.findAll();
@@ -47,8 +46,15 @@ export class ThreadsController {
     description: 'Ok',
     type: GetThreadResponseDto,
   })
-  @ApiOperation({ summary: 'Get thread', description: "Retrieves a thread along with its configurations." })
-  @ApiParam({ name: 'id', required: true, description: "The unique identifier of the thread." })
+  @ApiOperation({
+    summary: 'Get thread',
+    description: 'Retrieves a thread along with its configurations.',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'The unique identifier of the thread.',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.threadsService.findOne(id);
@@ -59,8 +65,15 @@ export class ThreadsController {
     description: 'The thread has been successfully updated.',
     type: UpdateThreadDto,
   })
-  @ApiOperation({ summary: 'Update thread', description: "Updates a thread's configurations." })
-  @ApiParam({ name: 'id', required: true, description: "The unique identifier of the thread." })
+  @ApiOperation({
+    summary: 'Update thread',
+    description: "Updates a thread's configurations.",
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'The unique identifier of the thread.',
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateThreadDto: UpdateThreadDto) {
     return this.threadsService.update(id, updateThreadDto);
@@ -71,8 +84,15 @@ export class ThreadsController {
     description: 'The thread has been successfully deleted.',
     type: DeleteThreadResponseDto,
   })
-  @ApiOperation({ summary: 'Delete thread', description: "Deletes a specific thread defined by a thread `id` ." })
-  @ApiParam({ name: 'id', required: true, description: "The unique identifier of the thread." })
+  @ApiOperation({
+    summary: 'Delete thread',
+    description: 'Deletes a specific thread defined by a thread `id` .',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'The unique identifier of the thread.',
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.threadsService.remove(id);
