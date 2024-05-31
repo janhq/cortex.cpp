bb5179224fcc932b86faf45e17d6764d20d3c4fe
Louis <louis@jan.ai>
2024-05-31 08:45:58 +0700

    fix: allow users to select a model to start (#633)

--- cortex-js/src/infrastructure/commanders/usecases/ps.cli.usecases.ts
+++ cortex-js/src/infrastructure/commanders/usecases/ps.cli.usecases.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { defaultCortexCppHost, defaultCortexCppPort } from 'constant';
 
-interface ModelStat {
+export interface ModelStat {
   modelId: string;
   engine?: string;
   duration?: string;
