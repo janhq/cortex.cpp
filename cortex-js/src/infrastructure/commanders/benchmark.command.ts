de351c688d9cc54a36a02f1de9723be83689679b
Louis <louis@jan.ai>
2024-06-13 14:40:26 +0700

    chore: add benchmark options (#690)

--- cortex-js/src/infrastructure/commanders/benchmark.command.ts
+++ cortex-js/src/infrastructure/commanders/benchmark.command.ts
@@ -1,5 +1,6 @@
-import { CommandRunner, SubCommand } from 'nest-commander';
+import { CommandRunner, SubCommand, Option } from 'nest-commander';
 import { BenchmarkCliUsecases } from './usecases/benchmark.cli.usecases';
+import { BenchmarkConfig } from './types/benchmark-config.interface';
 
 @SubCommand({
   name: 'benchmark',
@@ -12,7 +13,34 @@ export class BenchmarkCommand extends CommandRunner {
     super();
   }
 
-  async run(): Promise<void> {
-    return this.benchmarkUsecases.benchmark();
+  async run(
+    _input: string[],
+    options?: Partial<BenchmarkConfig>,
+  ): Promise<void> {
+    return this.benchmarkUsecases.benchmark(options ?? {});
+  }
+
+  @Option({
+    flags: '-n, --num_rounds <num_rounds>',
+    description: 'Number of rounds to run the benchmark',
+  })
+  parseRounds(value: number) {
+    return value;
+  }
+
+  @Option({
+    flags: '-c, --concurrency <concurrency>',
+    description: 'Number of concurrent requests to run the benchmark',
+  })
+  parseConcurrency(value: number) {
+    return value;
+  }
+
+  @Option({
+    flags: '-o, --output <output>',
+    description: 'Output format for the benchmark results. json or table',
+  })
+  parseOutput(value: string) {
+    return value;
   }
 }
