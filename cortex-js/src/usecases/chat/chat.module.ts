a96f797663687c8c36ac614a6b3977216689c34f
marknguyen1302 <nguyenvu1302.work@gmail.com>
2024-06-17 10:02:08 +0700

    fix missing import

--- cortex-js/src/usecases/chat/chat.module.ts
+++ cortex-js/src/usecases/chat/chat.module.ts
@@ -4,9 +4,16 @@ import { DatabaseModule } from '@/infrastructure/database/database.module';
 import { ExtensionModule } from '@/infrastructure/repositories/extensions/extension.module';
 import { ModelRepositoryModule } from '@/infrastructure/repositories/models/model.module';
 import { HttpModule } from '@nestjs/axios';
+import { TelemetryModule } from '../telemetry/telemetry.module';
 
 @Module({
-  imports: [DatabaseModule, ExtensionModule, ModelRepositoryModule, HttpModule],
+  imports: [
+    DatabaseModule,
+    ExtensionModule,
+    ModelRepositoryModule,
+    HttpModule,
+    TelemetryModule,
+  ],
   controllers: [],
   providers: [ChatUsecases],
   exports: [ChatUsecases],
