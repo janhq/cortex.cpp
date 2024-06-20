6589d59a3b6b00c786f0c1d82e755ed0800367af
James <namnh0122@gmail.com>
2024-06-20 16:07:38 +0700

    fix: test cases
    Signed-off-by: James <namnh0122@gmail.com>

--- cortex-js/src/usecases/assistants/assistants.usecases.spec.ts
+++ cortex-js/src/usecases/assistants/assistants.usecases.spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssistantsUsecases } from './assistants.usecases';
 import { DatabaseModule } from '@/infrastructure/database/database.module';
+import { ModelRepositoryModule } from '@/infrastructure/repositories/models/model.module';
+import { DownloadManagerModule } from '@/infrastructure/services/download-manager/download-manager.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 describe('AssistantsService', () => {
   let service: AssistantsUsecases;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule],
+      imports: [
+        EventEmitterModule.forRoot(),
+        DatabaseModule,
+        ModelRepositoryModule,
+        DownloadManagerModule,
+      ],
       exports: [AssistantsUsecases],
       providers: [AssistantsUsecases],
     }).compile();
