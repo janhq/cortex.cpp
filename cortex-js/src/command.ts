737e5f738b8c0b88b7e4df3ac1d8a2a41bf2fd1a
Louis <louis@jan.ai>
2024-06-25 21:59:23 +0700

    fix: cortex node suppress warning (#771)

--- cortex-js/src/command.ts
+++ cortex-js/src/command.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings
 import { CommandFactory } from 'nest-commander';
 import { CommandModule } from './command.module';
 import { TelemetryUsecases } from './usecases/telemetry/telemetry.usecases';
