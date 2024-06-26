e143638b64f429f3036c94a2e951b301769aab79
irfanpena <irfan@penateam.com>
2024-06-26 07:34:44 +0700

    Update the readme and the server URL for the API documentation

--- cortex-js/src/app.ts
+++ cortex-js/src/app.ts
@@ -62,7 +62,7 @@ export const getApp = async () => {
       'Events',
       'Endpoints for  observing Cortex statuses through event notifications.',
     )
-    .addServer('http://localhost:1337')
+    .addServer('http://localhost:1337/api')
     .addServer('http://localhost:1337/v1')
     .build();
   const document = SwaggerModule.createDocument(app, config);
