0cde51e42af36196e350d398cca9db5b1e0939a6
irfanpena <irfan@penateam.com>
2024-06-27 12:45:33 +0700

    Add example

--- cortex-js/src/infrastructure/dtos/chat/chat-completion-message.dto.ts
+++ cortex-js/src/infrastructure/dtos/chat/chat-completion-message.dto.ts
@@ -2,12 +2,13 @@ import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ChatCompletionMessage {
-  @ApiProperty({ description: 'The Content of the chat message.' })
+  @ApiProperty({ description: 'The Content of the chat message.', example: 'Hello World', })
   @IsString()
   content: string;
 
   @ApiProperty({
     description: 'The role of the entity in the chat completion.',
+    example: 'user',
   })
   role: 'user' | 'assistant';
 }
