400be006f46facffea5a403f4fc1118ec593ca9c
marknguyen1302 <nguyenvu1302.work@gmail.com>
2024-07-01 21:10:07 +0700

    remove telemetry

--- cortex-js/src/command.ts
+++ cortex-js/src/command.ts
@@ -1,42 +1,42 @@
 #!/usr/bin/env node --no-warnings
 import { CommandFactory } from 'nest-commander';
 import { CommandModule } from './command.module';
-import { TelemetryUsecases } from './usecases/telemetry/telemetry.usecases';
+// import { TelemetryUsecases } from './usecases/telemetry/telemetry.usecases';
 import { TelemetrySource } from './domain/telemetry/telemetry.interface';
 // import { AsyncLocalStorage } from 'async_hooks';
-import { ContextService } from '@/infrastructure/services/context/context.service';
+// import { ContextService } from '@/infrastructure/services/context/context.service';
 
 // export const asyncLocalStorage = new AsyncLocalStorage();
 console.time('test');
 console.log('Running CLI');
 async function bootstrap() {
-  let telemetryUseCase: TelemetryUsecases | null = null;
-  let contextService: ContextService | null = null;
+  // let telemetryUseCase: TelemetryUsecases | null = null;
+  // let contextService: ContextService | null = null;
   console.time('CLI');
-  const app = await CommandFactory.createWithoutRunning(CommandModule, {
-    logger: ['warn', 'error'],
-    errorHandler: async (error) => {
-      await telemetryUseCase!.createCrashReport(error, TelemetrySource.CLI);
-      process.exit(1);
-    },
-    serviceErrorHandler: async (error) => {
-      await telemetryUseCase!.createCrashReport(error, TelemetrySource.CLI);
-      process.exit(1);
-    },
-  });
+  // const app = await CommandFactory.createWithoutRunning(CommandModule, {
+  //   logger: ['warn', 'error'],
+  //   // errorHandler: async (error) => {
+  //   //   await telemetryUseCase!.createCrashReport(error, TelemetrySource.CLI);
+  //   //   process.exit(1);
+  //   // },
+  //   // serviceErrorHandler: async (error) => {
+  //   //   await telemetryUseCase!.createCrashReport(error, TelemetrySource.CLI);
+  //   //   process.exit(1);
+  //   // },
+  // });
 
-  telemetryUseCase = await app.resolve(TelemetryUsecases);
-  contextService = await app.resolve(ContextService);
+  // telemetryUseCase = await app.resolve(TelemetryUsecases);
+  // contextService = await app.resolve(ContextService);
 
-  telemetryUseCase!.sendCrashReport();
+  // telemetryUseCase!.sendCrashReport();
 
-  await contextService!.init(async () => {
-    contextService!.set('source', TelemetrySource.CLI);
-    await CommandFactory.runApplication(app);
-    // await CommandFactory.run(CommandModule, ['warn', 'error']);
+  // await contextService!.init(async () => {
+    // contextService!.set('source', TelemetrySource.CLI);
+    // await CommandFactory.runApplication(app);
+    await CommandFactory.run(CommandModule, ['warn', 'error']);
     console.timeEnd('CLI');
     console.timeEnd('test');
-  });
+  // });
 }
 
 bootstrap();
