112b3ac05fdfb34a73622643bd5a6fab1c2a84e1
marknguyen1302 <nguyenvu1302.work@gmail.com>
2024-07-01 20:47:53 +0700

    revert debug

--- cortex-js/src/infrastructure/commanders/ps.command.ts
+++ cortex-js/src/infrastructure/commanders/ps.command.ts
@@ -7,17 +7,21 @@ import { ContextService } from '../services/context/context.service';
   name: 'ps',
   description: 'Show running models and their status',
 })
-// @SetCommandContext()
+@SetCommandContext()
 export class PSCommand extends CommandRunner {
   constructor(
-    // private readonly usecases: PSCliUsecases,
-    // readonly contextService: ContextService,
+    private readonly usecases: PSCliUsecases,
+    readonly contextService: ContextService,
   ) {
     super();
   }
   async run(): Promise<void> {
-    console.log('Running ps command');
-    console.time('ps');
-    return;
+    return this.usecases
+      .getModels()
+      .then(console.table)
+      .then(() => this.usecases.isAPIServerOnline())
+      .then((isOnline) => {
+        if (isOnline) console.log('API server is online');
+      });
   }
 }
