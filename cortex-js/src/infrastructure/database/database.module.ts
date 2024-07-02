781498c2f079ae835e12a665aab467c47caf66b8
marknguyen1302 <nguyenvu1302.work@gmail.com>
2024-07-02 11:02:12 +0700

    revert database

--- cortex-js/src/infrastructure/database/database.module.ts
+++ cortex-js/src/infrastructure/database/database.module.ts
@@ -1,25 +1,25 @@
 import { Module } from '@nestjs/common';
-// import { threadProviders } from './providers/thread.providers';
-// import { sqliteDatabaseProviders } from './sqlite-database.providers';
-// import { assistantProviders } from './providers/assistant.providers';
-// import { messageProviders } from './providers/message.providers';
-// import { telemetryProviders } from './providers/telemetry.providers';
-// import { FileManagerModule } from '@/infrastructure/services/file-manager/file-manager.module';
+import { threadProviders } from './providers/thread.providers';
+import { sqliteDatabaseProviders } from './sqlite-database.providers';
+import { assistantProviders } from './providers/assistant.providers';
+import { messageProviders } from './providers/message.providers';
+import { telemetryProviders } from './providers/telemetry.providers';
+import { FileManagerModule } from '@/infrastructure/services/file-manager/file-manager.module';
 
 @Module({
   imports: [],
   providers: [
-    // ...sqliteDatabaseProviders,
-    // ...threadProviders,
-    // ...assistantProviders,
-    // ...messageProviders,
-    // ...telemetryProviders,
+    ...sqliteDatabaseProviders,
+    ...threadProviders,
+    ...assistantProviders,
+    ...messageProviders,
+    ...telemetryProviders,
   ],
   exports: [
-    // ...threadProviders,
-    // ...assistantProviders,
-    // ...messageProviders,
-    // ...telemetryProviders,
+    ...threadProviders,
+    ...assistantProviders,
+    ...messageProviders,
+    ...telemetryProviders,
   ],
 })
 export class DatabaseModule {}
