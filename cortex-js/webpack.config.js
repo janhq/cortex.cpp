f05c1c09d61f01e3a0baeb19c987582a24220eb6
marknguyen1302 <nguyenvu1302.work@gmail.com>
2024-07-03 23:42:46 +0700

    test webpack

--- cortex-js/webpack.config.js
+++ cortex-js/webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   entry: './src/command.ts',
   target: 'node',
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist/src'),
-    filename: 'command.js'
+    filename: 'main.js'
   },
   module: {
     rules: [
@@ -20,5 +24,19 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js'],
     plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })]
-  }
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new WebpackShellPluginNext({
+      onBuildEnd: {
+        scripts: ['echo "#!/usr/bin/env node" | cat - dist/src/main.js > temp && mv temp dist/src/command.js'],
+        blocking: true,
+        parallel: false
+      }
+    }),
+    new BundleAnalyzerPlugin()
+  ]
 };
