4c83e109f3c4eb460e35e2afcba34b23c031b61b
Louis <louis@jan.ai>
2024-07-08 14:27:40 +0700

    chore: update binding entry

---
+++ cortex-cpp/binding/index.js
@@ -0,0 +1,3 @@
+const addon = require("./../build/Release/cortex-cpp.node");
+
+module.exports = addon;
--- cortex-cpp/index.js
+++
@@ -1,7 +0,0 @@
-const addon = require("./build/Release/cortex-cpp.node");
-
-console.log(addon);
-
-console.log(addon.start());
-
-console.log(addon.stop());
