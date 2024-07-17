8cc2e9bddcfe36dd278b1d9e8fe6a8320a37ac91
marknguyen1302 <nguyenvu1302.work@gmail.com>
2024-07-17 15:13:14 +0700

    remove remote engine mapping

--- cortex-js/src/infrastructure/commanders/types/engine.interface.ts
+++ cortex-js/src/infrastructure/commanders/types/engine.interface.ts
@@ -14,8 +14,4 @@ export const EngineNamesMap = {
   [Engines.llamaCPP]: 'llamacpp',
   [Engines.onnx]: 'onnx',
   [Engines.tensorrtLLM]: 'tensorrt-llm',
-  [Engines.groq]: 'groq',
-  [Engines.mistral]: 'mistral',
-  [Engines.openai]: 'openai',
-  [Engines.anthropic]: 'anthropic',
 };
