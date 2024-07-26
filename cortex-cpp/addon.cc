1fab7dc329fe382155d3a7c5a507ad508b015899
vansangpfiev <vansangpfiev@gmail.com>
2024-07-26 12:38:26 +0700

    feat: add cortex-cpp version to log (#912)
    Co-authored-by: vansangpfiev <sang@jan.ai>

--- cortex-cpp/addon.cc
+++ cortex-cpp/addon.cc
@@ -36,9 +36,6 @@ void start() {
 #else
   LOG_INFO << "cortex-cpp version: undefined";
 #endif
-#ifdef CORTEX_LLAMACPP_VERSION
-  LOG_INFO << "cortex.llamacpp version: " << CORTEX_LLAMACPP_VERSION;
-#endif
 
   LOG_INFO << "Server started, listening at: " << host << ":" << port;
   LOG_INFO << "Please load your model";
