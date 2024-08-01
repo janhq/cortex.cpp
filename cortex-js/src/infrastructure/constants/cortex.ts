a34528031e3d0ec34e02326398776c86e8d53972
marknguyen1302 <nguyenvu1302.work@gmail.com>
2024-08-01 17:00:22 +0700

    fix: change default host cortex

--- cortex-js/src/infrastructure/constants/cortex.ts
+++ cortex-js/src/infrastructure/constants/cortex.ts
@@ -4,7 +4,7 @@ export const databaseName = 'cortex';
 
 export const databaseFile = `${databaseName}.db`;
 
-export const defaultCortexJsHost = 'localhost';
+export const defaultCortexJsHost = '127.0.0.1';
 export const defaultCortexJsPort = 1337;
 
 export const defaultCortexCppHost = '127.0.0.1';
