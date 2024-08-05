af5705d254a4d52d58986f4684ed2a92c971bc33
marknguyen1302 <nguyenvu1302.work@gmail.com>
2024-08-05 17:42:43 +0700

    ignore error file

--- cortex-js/src/utils/cortex-cpp.ts
+++ cortex-js/src/utils/cortex-cpp.ts
@@ -1,4 +1,8 @@
 import * as cortexCPP from 'cortex-cpp';
 
-const port = process.env.CORTEX_CPP_PORT ? parseInt(process.env.CORTEX_CPP_PORT) : 3929
+const port = process.env.CORTEX_CPP_PORT
+  ? parseInt(process.env.CORTEX_CPP_PORT)
+  : 3929;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 cortexCPP.start(port);
