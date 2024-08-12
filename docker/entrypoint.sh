a00d7a4cefa505684c73f10b65b729e9f72615fd
Hien To <tominhhien97@gmail.com>
2024-08-12 21:42:01 +0700

    Initiate Dockerfile

---
+++ docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Run cortex
+cortex  -a 0.0.0.0
+
+cortex engines llamacpp init
+cortex engines tensorrt-llm init
+
+# Keep the container running by tailing the log file
+tail -f /root/cortex/cortex.log
