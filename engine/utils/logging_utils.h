cbc1d623d07e84399ac6ea76fc9ec11682b79e07
vansangpfiev <vansangpfiev@gmail.com>
2024-09-04 08:56:36 +0700

    fix: more comments

--- engine/utils/logging_utils.h
+++ engine/utils/logging_utils.h
@@ -5,17 +5,19 @@
 // if not verbose only log result to console
 inline bool log_verbose = false;
 
+// Only use trantor log
 #define CTL_INF(msg) \
-  if (log_verbose) {    \
-    LOG_INFO << msg;    \
+  if (log_verbose) { \
+    LOG_INFO << msg; \
   }
 
 #define CTL_WRN(msg) \
-  if (log_verbose) {    \
-    LOG_WARN << msg;    \
+  if (log_verbose) { \
+    LOG_WARN << msg; \
   }
 
-#define CTL_ERR(msg)           \
+// Use std::cout if not verbose, use trantor log if verbose
+#define CTL_ERR(msg)               \
   if (log_verbose) {               \
     LOG_ERROR << msg;              \
   } else {                         \
