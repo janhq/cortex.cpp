c107bc94e4bb7e5f4b35445737bac84e7dcefcd9
vansangpfiev <vansangpfiev@gmail.com>
2024-09-09 17:03:46 +0700

    fix: clean resources unit tests

--- engine/test/components/test_cortex_upd_cmd.cc
+++ engine/test/components/test_cortex_upd_cmd.cc
@@ -2,11 +2,11 @@
 #include "gtest/gtest.h"
 
 namespace {
-    constexpr const auto kNewReleaseFolder = "./cortex-release";
-    constexpr const auto kNewReleaseFile = "./cortex-release/cortexexe";
-    constexpr const auto kCurReleaseFile = "./cortexexe";
-    constexpr const auto kTemp = "./cortex_temp";
-}
+constexpr const auto kNewReleaseFolder = "./cortex-release";
+constexpr const auto kNewReleaseFile = "./cortex-release/cortexexe";
+constexpr const auto kCurReleaseFile = "./cortexexe";
+constexpr const auto kTemp = "./cortex_temp";
+}  // namespace
 
 class CortexUpdCmdTest : public ::testing::Test {
   void SetUp() {
@@ -28,23 +28,27 @@ class CortexUpdCmdTest : public ::testing::Test {
     if (std::filesystem::exists(kCurReleaseFile)) {
       std::filesystem::remove(kCurReleaseFile);
     }
+
+    if (std::filesystem::exists(kTemp)) {
+      std::filesystem::remove(kTemp);
+    }
   }
 };
 
 TEST_F(CortexUpdCmdTest, return_true_if_self_replace) {
-    EXPECT_TRUE(commands::ReplaceBinaryInflight("test", "test"));
+  EXPECT_TRUE(commands::ReplaceBinaryInflight("test", "test"));
 }
 
 TEST_F(CortexUpdCmdTest, replace_binary_successfully) {
-    std::filesystem::path new_binary(kNewReleaseFile);
-    std::filesystem::path cur_binary(kCurReleaseFile);
-    EXPECT_TRUE(commands::ReplaceBinaryInflight(new_binary, cur_binary));
-    EXPECT_TRUE(std::filesystem::exists(kTemp));
+  std::filesystem::path new_binary(kNewReleaseFile);
+  std::filesystem::path cur_binary(kCurReleaseFile);
+  EXPECT_TRUE(commands::ReplaceBinaryInflight(new_binary, cur_binary));
+  EXPECT_TRUE(std::filesystem::exists(kTemp));
 }
 
 TEST_F(CortexUpdCmdTest, should_restore_old_binary_if_has_error) {
-    std::filesystem::path new_binary("Non-exist");
-    std::filesystem::path cur_binary(kCurReleaseFile);
-    EXPECT_FALSE(commands::ReplaceBinaryInflight(new_binary, cur_binary));
-    EXPECT_FALSE(std::filesystem::exists(kTemp));
+  std::filesystem::path new_binary("Non-exist");
+  std::filesystem::path cur_binary(kCurReleaseFile);
+  EXPECT_FALSE(commands::ReplaceBinaryInflight(new_binary, cur_binary));
+  EXPECT_FALSE(std::filesystem::exists(kTemp));
 }
