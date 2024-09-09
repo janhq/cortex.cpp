aa3162a7b341e7e08809bb0069efdea81e1c9568
Hien To <tominhhien97@gmail.com>
2024-09-09 17:15:43 +0700

    Remove cortex-cpp node

--- engine/binding/index.d.ts
+++
@@ -1,7 +0,0 @@
-// Type definitions for cortex-cpp node binding
-
-/// <reference types="node" />
-declare module "cortex-cpp" {
-  export function start(port?: number);
-  export function stop();
-}
