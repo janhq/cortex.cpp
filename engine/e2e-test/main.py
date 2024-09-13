82f7823aa95f6cb3893c4b5b6c1ec36d1e11e17c
nguyenhoangthuan99 <35255081+nguyenhoangthuan99@users.noreply.github.com>
2024-09-13 08:33:39 +0700

    Fix: create log folder before logging (#1200)
    * Fix create log folder before logging
    
    * Fix create log folder before logging
    
    * Fix create log folder before logging
    
    * Fix create log folder before logging
    
    * add end to end test for server start
    
    * add end to end test for log folder created
    
    * Update test_create_log_folder.py

--- engine/e2e-test/main.py
+++ engine/e2e-test/main.py
@@ -4,6 +4,8 @@ from test_cli_engine_get import TestCliEngineGet
 from test_cli_engine_install import TestCliEngineInstall
 from test_cli_engine_list import TestCliEngineList
 from test_cli_engine_uninstall import TestCliEngineUninstall
+from test_cli_server_start import TestCliServerStart
+from test_create_log_folder import TestCreateLogFolder
 
 if __name__ == "__main__":
     pytest.main([__file__, "-v"])
