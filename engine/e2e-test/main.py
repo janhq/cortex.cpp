effadfb4017e999d26ed00146d95541f2427655a
James <namnh0122@gmail.com>
2024-09-16 02:06:05 +0700

    feat: download model with direct HF url
    Signed-off-by: James <namnh0122@gmail.com>

--- engine/e2e-test/main.py
+++ engine/e2e-test/main.py
@@ -9,6 +9,7 @@ from test_cli_model_pull_direct_url import TestCliModelPullDirectUrl
 from test_cli_server_start import TestCliServerStart
 from test_cortex_update import TestCortexUpdate
 from test_create_log_folder import TestCreateLogFolder
+from test_cli_model_pull_direct_url import TestCliModelPullDirectUrl
 
 if __name__ == "__main__":
     pytest.main([__file__, "-v"])
