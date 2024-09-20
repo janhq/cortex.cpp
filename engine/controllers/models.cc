acf831fbc4619eb7de1bfe882e87e87487512aa5
Thuandz <thuanhppro12@gmail.com>
2024-09-20 10:44:51 +0700

    Fix comment

--- engine/controllers/models.cc
+++ engine/controllers/models.cc
@@ -67,69 +67,7 @@ void Models::ListModel(
 
         yaml_handler.ModelConfigFromFile(model_entry.path_to_model_yaml);
         auto model_config = yaml_handler.GetModelConfig();
-        Json::Value obj;
-        obj["id"] = model_config.id;
-        obj["name"] = model_config.name;
-        obj["model"] = model_config.model;
-        obj["version"] = model_config.version;
-
-        Json::Value stop_array(Json::arrayValue);
-        for (const std::string& stop : model_config.stop)
-          stop_array.append(stop);
-        obj["stop"] = stop_array;
-
-        obj["stream"] = model_config.stream;
-        obj["top_p"] = model_config.top_p;
-        obj["temperature"] = model_config.temperature;
-        obj["frequency_penalty"] = model_config.frequency_penalty;
-        obj["presence_penalty"] = model_config.presence_penalty;
-        obj["max_tokens"] = static_cast<int>(model_config.max_tokens);
-
-        // New fields
-        obj["seed"] = model_config.seed;
-        obj["dynatemp_range"] = model_config.dynatemp_range;
-        obj["dynatemp_exponent"] = model_config.dynatemp_exponent;
-        obj["top_k"] = model_config.top_k;
-        obj["min_p"] = model_config.min_p;
-        obj["tfs_z"] = model_config.tfs_z;
-        obj["typ_p"] = model_config.typ_p;
-        obj["repeat_last_n"] = model_config.repeat_last_n;
-        obj["repeat_penalty"] = model_config.repeat_penalty;
-        obj["mirostat"] = model_config.mirostat;
-        obj["mirostat_tau"] = model_config.mirostat_tau;
-        obj["mirostat_eta"] = model_config.mirostat_eta;
-        obj["penalize_nl"] = model_config.penalize_nl;
-        obj["ignore_eos"] = model_config.ignore_eos;
-        obj["n_probs"] = model_config.n_probs;
-        obj["min_keep"] = model_config.min_keep;
-
-        obj["ngl"] = model_config.ngl;
-        obj["ctx_len"] = static_cast<int>(model_config.ctx_len);
-        obj["engine"] = model_config.engine;
-        obj["prompt_template"] = model_config.prompt_template;
-        obj["system_template"] = model_config.system_template;
-        obj["user_template"] = model_config.user_template;
-        obj["ai_template"] = model_config.ai_template;
-
-        obj["os"] = model_config.os;
-        obj["gpu_arch"] = model_config.gpu_arch;
-        obj["quantization_method"] = model_config.quantization_method;
-        obj["precision"] = model_config.precision;
-
-        Json::Value files_array(Json::arrayValue);
-        for (const std::string& file : model_config.files)
-          files_array.append(file);
-        obj["files"] = files_array;
-
-        obj["created"] = static_cast<uint32_t>(model_config.created);
-        obj["object"] = model_config.object;
-        obj["owned_by"] = model_config.owned_by;
-        obj["text_model"] = model_config.text_model;
-
-        if (model_config.engine == "cortex.tensorrt-llm") {
-          obj["trtllm_version"] = model_config.trtllm_version;
-          obj["tp"] = model_config.tp;
-        }
+        Json::Value obj = model_config.ToJson();
 
         data.append(std::move(obj));
         yaml_handler.Reset();
@@ -175,70 +113,8 @@ void Models::GetModel(
     yaml_handler.ModelConfigFromFile(model_entry.path_to_model_yaml);
     auto model_config = yaml_handler.GetModelConfig();
 
-    Json::Value obj;
-    obj["id"] = model_config.id;
-    obj["name"] = model_config.name;
-    obj["model"] = model_config.model;
-    obj["version"] = model_config.version;
-
-    Json::Value stop_array(Json::arrayValue);
-    for (const std::string& stop : model_config.stop)
-      stop_array.append(stop);
-    obj["stop"] = stop_array;
-
-    obj["stream"] = model_config.stream;
-    obj["top_p"] = model_config.top_p;
-    obj["temperature"] = model_config.temperature;
-    obj["frequency_penalty"] = model_config.frequency_penalty;
-    obj["presence_penalty"] = model_config.presence_penalty;
-    obj["max_tokens"] = static_cast<int>(model_config.max_tokens);
-
-    // New fields
-    obj["seed"] = model_config.seed;
-    obj["dynatemp_range"] = model_config.dynatemp_range;
-    obj["dynatemp_exponent"] = model_config.dynatemp_exponent;
-    obj["top_k"] = model_config.top_k;
-    obj["min_p"] = model_config.min_p;
-    obj["tfs_z"] = model_config.tfs_z;
-    obj["typ_p"] = model_config.typ_p;
-    obj["repeat_last_n"] = model_config.repeat_last_n;
-    obj["repeat_penalty"] = model_config.repeat_penalty;
-    obj["mirostat"] = model_config.mirostat;
-    obj["mirostat_tau"] = model_config.mirostat_tau;
-    obj["mirostat_eta"] = model_config.mirostat_eta;
-    obj["penalize_nl"] = model_config.penalize_nl;
-    obj["ignore_eos"] = model_config.ignore_eos;
-    obj["n_probs"] = model_config.n_probs;
-    obj["min_keep"] = model_config.min_keep;
-
-    obj["ngl"] = model_config.ngl;
-    obj["ctx_len"] = static_cast<int>(model_config.ctx_len);
-    obj["engine"] = model_config.engine;
-    obj["prompt_template"] = model_config.prompt_template;
-    obj["system_template"] = model_config.system_template;
-    obj["user_template"] = model_config.user_template;
-    obj["ai_template"] = model_config.ai_template;
-
-    obj["os"] = model_config.os;
-    obj["gpu_arch"] = model_config.gpu_arch;
-    obj["quantization_method"] = model_config.quantization_method;
-    obj["precision"] = model_config.precision;
-
-    Json::Value files_array(Json::arrayValue);
-    for (const std::string& file : model_config.files)
-      files_array.append(file);
-    obj["files"] = files_array;
-
-    obj["created"] = static_cast<uint32_t>(model_config.created);
-    obj["object"] = model_config.object;
-    obj["owned_by"] = model_config.owned_by;
-    obj["text_model"] = model_config.text_model;
-
-    if (model_config.engine == "cortex.tensorrt-llm") {
-      obj["trtllm_version"] = model_config.trtllm_version;
-      obj["tp"] = model_config.tp;
-    }
-
+    Json::Value obj = model_config.ToJson();
+    
     data.append(std::move(obj));
     ret["data"] = data;
     ret["result"] = "OK";
@@ -336,7 +212,6 @@ void Models::ImportModel(
     }
 
   } catch (const std::exception& e) {
-    std::remove(model_yaml_path.c_str());
     std::string error_message = "Error importing model path '" + modelPath +
                                 "' with model_id '" + modelHandle +
                                 "': " + e.what();
