1e7c2acfe977071d799f082aa59eadcc1f2f4b0f
vansangpfiev <vansangpfiev@gmail.com>
2024-09-23 15:35:12 +0700

    feat: install local engine (#1292)
    * temp
    
    * feat: next
    
    * feat: more
    
    * fix: add e2e tests
    
    * fix: refactor
    
    * fix: build ubuntu
    
    * fix: explicit constructor
    
    * fix: comments
    
    * fix: cuda version
    
    * fix: jan_host
    
    * fix: correct cuda version

--- engine/services/engine_service.h
+++ engine/services/engine_service.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <memory>
 #include <optional>
 #include <string>
 #include <string_view>
 #include <vector>
+#include "utils/cpuid/cpu_info.h"
 
 struct EngineInfo {
   std::string name;
@@ -14,6 +16,9 @@ struct EngineInfo {
   std::string status;
 };
 
+namespace system_info_utils {
+struct SystemInfo;
+}
 class EngineService {
  public:
   constexpr static auto kIncompatible = "Incompatible";
@@ -23,12 +28,35 @@ class EngineService {
   const std::vector<std::string_view> kSupportEngines = {
       "cortex.llamacpp", "cortex.onnx", "cortex.tensorrt-llm"};
 
+  EngineService();
+  ~EngineService();
+
   std::optional<EngineInfo> GetEngineInfo(const std::string& engine) const;
 
   std::vector<EngineInfo> GetEngineInfoList() const;
 
   void InstallEngine(const std::string& engine,
-                     const std::string& version = "latest");
+                     const std::string& version = "latest",
+                     const std::string& src = "");
+
+  void UnzipEngine(const std::string& engine, const std::string& version,
+                   const std::string& path);
 
   void UninstallEngine(const std::string& engine);
+
+ private:
+  void DownloadEngine(const std::string& engine,
+                      const std::string& version = "latest");
+  void DownloadCuda(const std::string& engine);
+
+  std::string GetMatchedVariant(const std::string& engine,
+                                const std::vector<std::string>& variants);
+
+ private:
+  struct HardwareInfo {
+    std::unique_ptr<system_info_utils::SystemInfo> sys_inf;
+    cortex::cpuid::CpuInfo cpu_inf;
+    std::string cuda_driver_version;
+  };
+  HardwareInfo hw_inf_;
 };
