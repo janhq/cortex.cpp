7fc7d38af55f5af31f2a1a48aadf410f63a39cf9
vansangpfiev <vansangpfiev@gmail.com>
2024-10-02 16:28:02 +0700

    fix: e2e tests

--- engine/e2e-test/test_api_model_start.py
+++ engine/e2e-test/test_api_model_start.py
@@ -23,6 +23,6 @@ class TestApiModelStart:
         stop_server()
 
     def test_models_start_should_be_successful(self):
-        json_body = {"model": "tinyllama:gguf"}
+        json_body = {'model': 'tinyllama:gguf'}
         response = requests.post("http://localhost:3928/models/start", json = json_body)
-        assert response.status_code == 200
+        assert response.status_code == 200, f"status_code: {response.status_code}"
