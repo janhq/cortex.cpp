1cc978353b42889a7d2c767b0d7290aec2b96e02
vansangpfiev <vansangpfiev@gmail.com>
2024-10-03 12:25:52 +0700

    fix: comments

--- engine/config/yaml_config.cc
+++ engine/config/yaml_config.cc
@@ -289,9 +289,7 @@ void YamlHandler::WriteYamlFile(const std::string& file_path) const {
       outFile << "version: " << yaml_node_["version"].as<std::string>() << "\n";
     }
     if (yaml_node_["files"] && yaml_node_["files"].size()) {
-      outFile << "files:             # can be universal protocol (models://) "
-                 "OR absolute local file path (file://) OR https remote URL "
-                 "(https://)\n";
+      outFile << "files:             # Can be relative OR absolute local file path\n";
       for (const auto& source : yaml_node_["files"]) {
         outFile << "  - " << source << "\n";
       }
