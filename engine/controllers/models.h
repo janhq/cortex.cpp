dbbd233605be275a1c7efed6e9b2ccb6c19ecd2b
James <namnh0122@gmail.com>
2024-10-16 16:00:50 +0700

    fix: map v1 for abort model API

--- engine/controllers/models.h
+++ engine/controllers/models.h
@@ -22,7 +22,7 @@ class Models : public drogon::HttpController<Models, false> {
   METHOD_ADD(Models::GetModelStatus, "/status/{1}", Get);
 
   ADD_METHOD_TO(Models::PullModel, "/v1/models/pull", Post);
-  ADD_METHOD_TO(Models::PullModel, "/v1/models/pull", Delete);
+  ADD_METHOD_TO(Models::AbortPullModel, "/v1/models/pull", Delete);
   ADD_METHOD_TO(Models::ListModel, "/v1/models", Get);
   ADD_METHOD_TO(Models::GetModel, "/v1/models/{1}", Get);
   ADD_METHOD_TO(Models::UpdateModel, "/v1/models/{1}", Patch);
