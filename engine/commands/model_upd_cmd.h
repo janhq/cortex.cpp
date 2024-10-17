c4272a68660d68ea5b31faead611274337326cb7
vansangpfiev <vansangpfiev@gmail.com>
2024-10-17 11:06:18 +0700

    fix: model update command only accept string type (#1510)
    * fix: revert check type for json on model update command
    
    * fix: build
    
    ---------
    
    Co-authored-by: vansangpfiev <sang@jan.ai>

--- engine/commands/model_upd_cmd.h
+++ engine/commands/model_upd_cmd.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <functional>
+#include "json/json.h"
 namespace commands {
 class ModelUpdCmd {
  public:
@@ -11,6 +13,18 @@ class ModelUpdCmd {
 
   void Exec(const std::string& host, int port,
             const std::unordered_map<std::string, std::string>& options);
+
+ private:
+  void UpdateConfig(Json::Value& data, const std::string& key,
+                    const std::string& value);
+  void UpdateVectorField(
+      const std::string& key, const std::string& value,
+      std::function<void(const std::vector<std::string>&)> setter);
+  void UpdateNumericField(const std::string& key, const std::string& value,
+                          std::function<void(float)> setter);
+  void UpdateBooleanField(const std::string& key, const std::string& value,
+                          std::function<void(bool)> setter);
+
  private:
   std::string model_handle_;
 };
