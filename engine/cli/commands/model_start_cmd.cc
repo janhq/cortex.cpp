d449b151740ff7445a25b0a46d921e51c649ed8f
vansangpfiev <vansangpfiev@gmail.com>
2024-10-22 13:07:27 +0700

    fix: cortex models start has no output if variant not given (#1531)
    * fix: use engine get env
    
    * fix: cortex models start has no output if variant not given
    
    ---------
    
    Co-authored-by: vansangpfiev <sang@jan.ai>

--- engine/cli/commands/model_start_cmd.cc
+++ engine/cli/commands/model_start_cmd.cc
@@ -1,11 +1,23 @@
 #include "model_start_cmd.h"
+#include "config/yaml_config.h"
+#include "cortex_upd_cmd.h"
+#include "database/models.h"
 #include "httplib.h"
+#include "run_cmd.h"
 #include "server_start_cmd.h"
+#include "utils/cli_selection_utils.h"
 #include "utils/logging_utils.h"
 
 namespace commands {
 bool ModelStartCmd::Exec(const std::string& host, int port,
                          const std::string& model_handle) {
+  std::optional<std::string> model_id =
+      SelectLocalModel(model_service_, model_handle);
+
+  if(!model_id.has_value()) {
+    return false;
+  }
+
   // Start server if server is not started yet
   if (!commands::IsServerAlive(host, port)) {
     CLI_LOG("Starting server ...");
@@ -17,14 +29,16 @@ bool ModelStartCmd::Exec(const std::string& host, int port,
   // Call API to start model
   httplib::Client cli(host + ":" + std::to_string(port));
   Json::Value json_data;
-  json_data["model"] = model_handle;
+  json_data["model"] = model_id.value();
   auto data_str = json_data.toStyledString();
   cli.set_read_timeout(std::chrono::seconds(60));
   auto res = cli.Post("/v1/models/start", httplib::Headers(), data_str.data(),
                       data_str.size(), "application/json");
   if (res) {
     if (res->status == httplib::StatusCode::OK_200) {
-      CLI_LOG("Model loaded!");
+      CLI_LOG(model_id.value() << " model started successfully. Use `"
+                               << commands::GetCortexBinary() << " run "
+                               << *model_id << "` for interactive chat shell");
       return true;
     } else {
       CTL_ERR("Model failed to load with status code: " << res->status);
