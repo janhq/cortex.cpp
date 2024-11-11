135c41ededef9bcd61e4379eabee0c8590ba3e1b
nguyenhoangthuan99 <thuanhppro12@gmail.com>
2024-11-12 00:02:28 +0700

    Fix: CI build windows

--- engine/extensions/remote-engine/TemplateRenderer.h
+++ engine/extensions/remote-engine/TemplateRenderer.h
@@ -1,28 +1,32 @@
 #pragma once
 
-#include <string>
-#include <memory>
+// clang-format off
 #include <nlohmann/json.hpp>
 #include <inja/inja.hpp>
+// clang-format on
+#include <memory>
+
+#include <string>
 #include "json/json.h"
 #include "trantor/utils/Logger.h"
 class TemplateRenderer {
-public:
-    TemplateRenderer();
-    ~TemplateRenderer() = default;
+ public:
+  TemplateRenderer();
+  ~TemplateRenderer() = default;
 
-    // Convert Json::Value to nlohmann::json
-    static nlohmann::json convertJsonValue(const Json::Value& input);
-    
-    // Convert nlohmann::json to Json::Value
-    static Json::Value convertNlohmannJson(const nlohmann::json& input);
+  // Convert Json::Value to nlohmann::json
+  static nlohmann::json convertJsonValue(const Json::Value& input);
 
-    // Render template with data
-    std::string render(const std::string& tmpl, const Json::Value& data);
+  // Convert nlohmann::json to Json::Value
+  static Json::Value convertNlohmannJson(const nlohmann::json& input);
 
-    // Load template from file and render
-    std::string renderFile(const std::string& template_path, const Json::Value& data);
+  // Render template with data
+  std::string render(const std::string& tmpl, const Json::Value& data);
 
-private:
-    inja::Environment env_;
+  // Load template from file and render
+  std::string renderFile(const std::string& template_path,
+                         const Json::Value& data);
+
+ private:
+  inja::Environment env_;
 };
