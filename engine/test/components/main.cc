e8a8de55beb3ff0e4859190c8676f69377ad3624
vansangpfiev <sang@jan.ai>
2024-11-13 09:43:33 +0700

    chore: move FileManagerConfigTest test to the end

--- engine/test/components/main.cc
+++ engine/test/components/main.cc
@@ -1,9 +1,14 @@
-#include "gtest/gtest.h"
 #include <drogon/HttpAppFramework.h>
 #include <drogon/drogon.h>
+#include "gtest/gtest.h"
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    int ret = RUN_ALL_TESTS();
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  ::testing::GTEST_FLAG(filter) = "-FileManagerConfigTest.*";
+  int ret = RUN_ALL_TESTS();
+  if (ret != 0)
     return ret;
+  ::testing::GTEST_FLAG(filter) = "FileManagerConfigTest.*";
+  ret = RUN_ALL_TESTS();
+  return ret;
 }
