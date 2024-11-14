d9376ed1b7a5529e3b9fca17d02ff5e61bb4538d
Hien To <tominhhien97@gmail.com>
2024-11-14 17:27:59 +0700

    feat: linux bash install script support both .deb and binary

--- engine/templates/linux/install.sh
+++ engine/templates/linux/install.sh
@@ -6,6 +6,14 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
+# Determine the home directory based on the user
+USER_TO_RUN_AS=${SUDO_USER:-$(whoami)}
+if [ "$USER_TO_RUN_AS" = "root" ]; then
+    USER_HOME="/root"
+else
+    USER_HOME="/home/$USER_TO_RUN_AS"
+fi
+
 # Check and suggest installing jq and tar if not present
 check_install_jq_tar() {
     RED='\033[0;31m'
@@ -47,18 +55,11 @@ get_latest_version() {
   echo "${tag_name#v}"
 }
 
-# Determine the home directory based on the user
-USER_TO_RUN_AS=${SUDO_USER:-$(whoami)}
-if [ "$USER_TO_RUN_AS" = "root" ]; then
-    USER_HOME="/root"
-else
-    USER_HOME="/home/$USER_TO_RUN_AS"
-fi
-
 # Default values
 CHANNEL="stable"
 VERSION=""
 IS_UPDATE="false"
+DEB_LOCAL="false"
 
 # Function to parse command-line arguments
 parse_args() {
@@ -71,6 +72,10 @@ parse_args() {
       --version)
         VERSION="$2"
         shift 2
+        ;;
+      --deb_local)
+        DEB_LOCAL="true"
+        shift 1
         ;;
       --is_update)
         IS_UPDATE="true"
@@ -99,24 +104,24 @@ case $CHANNEL in
     SERVER_BINARY_NAME="cortex-server"
     DATA_DIR="$USER_HOME/cortexcpp"
     UNINSTALL_SCRIPT="/usr/bin/cortex-uninstall.sh"
-    UPDATER_SCRIPT="/usr/bin/cortex-updater.sh"
     CONFIGURATION_FILE="$USER_HOME/.cortexrc"
+    DEB_APP_NAME="cortexcpp"
     ;;
   beta)
     CLI_BINARY_NAME="cortex-beta"
     SERVER_BINARY_NAME="cortex-server-beta"
     DATA_DIR="$USER_HOME/cortexcpp-beta"
     UNINSTALL_SCRIPT="/usr/bin/cortex-beta-uninstall.sh"
-    UPDATER_SCRIPT="/usr/bin/cortex-beta-updater.sh"
     CONFIGURATION_FILE="$USER_HOME/.cortexrc-beta"
+    DEB_APP_NAME="cortexcpp-beta"
     ;;
   nightly)
     CLI_BINARY_NAME="cortex-nightly"
     SERVER_BINARY_NAME="cortex-server-nightly"
     DATA_DIR="$USER_HOME/cortexcpp-nightly"
     UNINSTALL_SCRIPT="/usr/bin/cortex-nightly-uninstall.sh"
-    UPDATER_SCRIPT="/usr/bin/cortex-nightly-updater.sh"
     CONFIGURATION_FILE="$USER_HOME/.cortexrc-nightly"
+    DEB_APP_NAME="cortexcpp-nightly"
     ;;
   *)
     echo "Invalid channel specified."
@@ -130,34 +135,67 @@ INSTALL_DIR="/usr/bin"
 install_cortex() {
   local channel=$1
   local version=$2
-  local url=""
+  local is_deb=$3
+  local url_binary=""
+  local url_deb_local=""
+  local url_deb_network=""
 
   case $channel in
     stable)
-      url="https://github.com/janhq/cortex.cpp/releases/download/v${version}/cortex-${version}-linux-amd64.tar.gz"
+      url_binary="https://github.com/janhq/cortex.cpp/releases/download/v${version}/cortex-${version}-linux-amd64.tar.gz"
+      url_deb_local="https://github.com/janhq/cortex.cpp/releases/download/v${version}/cortex-${version}-linux-amd64-local-installer.deb"
+      url_deb_network="https://github.com/janhq/cortex.cpp/releases/download/v${version}/cortex-${version}-linux-amd64-network-installer.deb"
       ;;
     beta)
-      url="https://github.com/janhq/cortex.cpp/releases/download/v${version}/cortex-${version}-linux-amd64.tar.gz"
+      url_binary="https://github.com/janhq/cortex.cpp/releases/download/v${version}/cortex-${version}-linux-amd64.tar.gz"
+      url_deb_local="https://github.com/janhq/cortex.cpp/releases/download/v${version}/cortex-${version}-linux-amd64-local-installer.deb"
+      url_deb_network="https://github.com/janhq/cortex.cpp/releases/download/v${version}/cortex-${version}-linux-amd64-network-installer.deb"
       ;;
     nightly)
-      url="https://delta.jan.ai/cortex/v${version}/linux-amd64/cortex-nightly.tar.gz"
+      url_binary="https://delta.jan.ai/cortex/v${version}/linux-amd64/cortex-nightly.tar.gz"
+      url_deb_local="https://delta.jan.ai/cortex/v${version}/linux-amd64/cortex-${version}-linux-amd64-local-installer.deb"
+      url_deb_network="https://delta.jan.ai/cortex/v${version}/linux-amd64/cortex-${version}-linux-amd64-network-installer.deb"
       ;;
   esac
-  echo "Downloading cortex $channel version $version from $url"
-  curl -L $url -o /tmp/cortex.tar.gz
-  tar -xzvf /tmp/cortex.tar.gz -C /tmp
-  chmod +x /tmp/cortex/*
-  cp /tmp/cortex/* /usr/bin/
-  # Check is update or not
-  if [ "$IS_UPDATE" = "false" ]; then
-    su -c "$INSTALL_DIR/$CLI_BINARY_NAME engines install llama-cpp" $USER_TO_RUN_AS
-    su -c "$INSTALL_DIR/$CLI_BINARY_NAME stop > /dev/null 2>&1" $USER_TO_RUN_AS
+
+  if [ "$is_deb" = "true" ]; then
+    # Download the deb package
+    if [ "$DEB_LOCAL" = "true" ]; then
+      echo "Downloading cortex $channel version $version from $url_deb_local"
+      curl -L $url_deb_local -o /tmp/cortex.deb
+    else
+      echo "Downloading cortex $channel version $version from $url_deb_network"
+      curl -L $url_deb_network -o /tmp/cortex.deb
+    fi
+
+    # Install the deb package
+    if [ "$IS_UPDATE" = "false" ]; then
+      apt-get install -y /tmp/cortex.deb
+    else
+      echo -e "n\n" | SKIP_POSTINSTALL=true apt-get install -y --allow-downgrades /tmp/cortex.deb
+    fi
+    rm -f /tmp/cortex.deb
+  else
+    echo "Downloading cortex $channel version $version from $url_binary"
+    curl -L $url_binary -o /tmp/cortex.tar.gz
+    tar -xzvf /tmp/cortex.tar.gz -C /tmp
+    chmod +x /tmp/cortex/*
+    cp /tmp/cortex/* /usr/bin/
+    # Check is update or not
+    if [ "$IS_UPDATE" = "false" ]; then
+      su -c "$INSTALL_DIR/$CLI_BINARY_NAME engines install llama-cpp" $USER_TO_RUN_AS
+      su -c "$INSTALL_DIR/$CLI_BINARY_NAME stop > /dev/null 2>&1" $USER_TO_RUN_AS
+    fi
+    rm -rf /tmp/cortex
+    rm -f /tmp/cortex.tar.gz
   fi
 }
 
 # Function to create uninstall script
 create_uninstall_script() {
-  cat << EOF > $UNINSTALL_SCRIPT
+  local is_deb=$1
+  if [ "$is_deb" = "false" ]; then
+    cat << EOF > $UNINSTALL_SCRIPT
 #!/bin/bash
 # Check for root privileges
 if [ "\$(id -u)" != "0" ]; then
@@ -167,12 +205,11 @@ fi
 
 echo "Stopping cortex..."
 su -c "$INSTALL_DIR/$CLI_BINARY_NAME stop > /dev/null 2>&1" $USER_TO_RUN_AS
-rm -rf $INSTALL_DIR/$CLI_BINARY_NAME
-rm -rf $INSTALL_DIR/$SERVER_BINARY_NAME
+rm -f $INSTALL_DIR/$CLI_BINARY_NAME
+rm -f $INSTALL_DIR/$SERVER_BINARY_NAME
 rm -f $UNINSTALL_SCRIPT
-rm -f $UPDATER_SCRIPT
 
-echo "Do you want to delete the ~/$DATA_DIR data folder and file ~/$CONFIGURATION_FILE? (yes/no) [default: no]"
+echo "Do you want to delete the $DATA_DIR data folder and file $CONFIGURATION_FILE? (yes/no) [default: no]"
 read -r answer
 while true; do
     case "\$answer" in
@@ -200,31 +237,45 @@ while true; do
 done
 
 EOF
-  chmod +x $UNINSTALL_SCRIPT
-  echo "Uninstall script created at $UNINSTALL_SCRIPT"
-}
 
-# Function to create updater script
-create_updater_script() {
-  cat << EOF > $UPDATER_SCRIPT
+  else
+    cat << EOF > $UNINSTALL_SCRIPT
 #!/bin/bash
 # Check for root privileges
 if [ "\$(id -u)" != "0" ]; then
     echo "This script must be run as root. Please run again with sudo."
     exit 1
 fi
-echo "Stopping cortex for update..."
-su -c "$INSTALL_DIR/$CLI_BINARY_NAME stop > /dev/null 2>&1" $USER_TO_RUN_AS
-curl -s https://raw.githubusercontent.com/janhq/cortex/feat/linux-bash-install-script/engine/templates/linux/install.sh | bash -s -- --channel $CHANNEL --is_update
+
+apt-get remove -y $DEB_APP_NAME
+rm -f $UNINSTALL_SCRIPT
 EOF
-  chmod +x $UPDATER_SCRIPT
-  echo "Updater script created at $UPDATER_SCRIPT"
+  fi
+
+  chmod +x $UNINSTALL_SCRIPT
+  echo "Uninstall script created at $UNINSTALL_SCRIPT"
 }
 
 # Run installation
 check_install_jq_tar
-install_cortex $CHANNEL $VERSION
-create_uninstall_script
-create_updater_script
 
-echo "Installation complete. Run cortex-uninstall.sh to uninstall and cortex-updater.sh to update."
+IS_DEB="false"
+
+# Check if apt-get command is available
+if command -v apt-get &> /dev/null; then
+  if [ "$IS_UPDATE" = "true" ]; then
+    # check if cortexcpp deb package is installed
+    if dpkg -l | grep -q $DEB_APP_NAME; then
+      IS_DEB="true"
+    else
+      IS_DEB="false"
+    fi
+  else
+    IS_DEB="true"
+  fi
+fi
+
+install_cortex $CHANNEL $VERSION $IS_DEB
+create_uninstall_script $IS_DEB
+
+echo "Installation complete. Run cortex-uninstall.sh to uninstall."
