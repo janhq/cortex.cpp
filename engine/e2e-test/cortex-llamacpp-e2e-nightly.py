d2bf352ec58af2a8d5f8a55b4f287c338bed3873
Hien To <tominhhien97@gmail.com>
2024-12-06 12:40:40 +0700

    fix: cortex.cpp nightly test with cortex.llamacpp

--- engine/e2e-test/cortex-llamacpp-e2e-nightly.py
+++ engine/e2e-test/cortex-llamacpp-e2e-nightly.py
@@ -3,31 +3,20 @@ import sys
 
 ### e2e tests are expensive, have to keep engines tests in order
 from test_api_engine_list import TestApiEngineList
-from test_api_engine_install_nightly import TestApiEngineInstall
-from test_api_engine_get import TestApiEngineGet
-
-### models, keeps in order, note that we only uninstall engine after finishing all models test
-from test_api_model_pull_direct_url import TestApiModelPullDirectUrl
-from test_api_model_start import TestApiModelStart
-from test_api_model_stop import TestApiModelStop
-from test_api_model_get import TestApiModelGet
-from test_api_model_list import TestApiModelList
-from test_api_model_update import TestApiModelUpdate
-from test_api_model_delete import TestApiModelDelete
+from test_cli_engine_install_nightly import TestApiEngine
+from test_api_model import TestApiModel
 from test_api_model_import import TestApiModelImport
-from test_api_engine_uninstall import TestApiEngineUninstall
 
 ###
 from test_cli_engine_get import TestCliEngineGet
 from test_cli_engine_install_nightly import TestCliEngineInstall
 from test_cli_engine_list import TestCliEngineList
-from test_cli_model_delete import TestCliModelDelete
-from test_cli_model_pull_direct_url import TestCliModelPullDirectUrl
+from test_cli_engine_uninstall import TestCliEngineUninstall
+from test_cli_model import TestCliModel
 from test_cli_server_start import TestCliServerStart
 from test_cortex_update import TestCortexUpdate
 from test_create_log_folder import TestCreateLogFolder
 from test_cli_model_import import TestCliModelImport
-from test_cli_engine_uninstall import TestCliEngineUninstall
 
 if __name__ == "__main__":
     sys.exit(pytest.main([__file__, "-v"]))
