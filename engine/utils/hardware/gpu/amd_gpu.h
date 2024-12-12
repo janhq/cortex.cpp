ae15bceab0bd642ffb89eb7377332a6cce2f86e8
vansangpfiev <sang@jan.ai>
2024-12-12 08:42:20 +0700

    chore: remove unused code

--- engine/utils/hardware/gpu/amd_gpu.h
+++ engine/utils/hardware/gpu/amd_gpu.h
@@ -172,26 +172,12 @@ void Test() {
   int displayIndex = 0;
   DISPLAY_DEVICEA displayDevice = {};
   displayDevice.cb = sizeof(displayDevice);
-  while (EnumDisplayDevicesA(0, displayIndex, &displayDevice, 0)) {
-    printf(
-        "Display Device: %d: %s, %s %s%s\n", displayIndex,
-        displayDevice.DeviceString, displayDevice.DeviceName,
-        displayDevice.StateFlags & DISPLAY_DEVICE_PRIMARY_DEVICE ? "(primary)"
-                                                                 : "",
-        displayDevice.StateFlags & DISPLAY_DEVICE_ACTIVE ? "" : " [disabled]");
-    displayIndex++;
-  }
 
   AGSContext* agsContext = nullptr;
   AGSGPUInfo gpuInfo = {};
   AGSConfiguration config = {};
   if (agsInitialize(AGS_CURRENT_VERSION, &config, &agsContext, &gpuInfo) ==
       AGS_SUCCESS) {
-    printf("\nAGS Library initialized: v%d.%d.%d\n", AMD_AGS_VERSION_MAJOR,
-           AMD_AGS_VERSION_MINOR, AMD_AGS_VERSION_PATCH);
-    printf(
-        "-----------------------------------------------------------------\n");
-
     printf("Radeon Software Version:   %s\n", gpuInfo.radeonSoftwareVersion);
     printf("Driver Version:            %s\n", gpuInfo.driverVersion);
     printf(
@@ -199,18 +185,6 @@ void Test() {
     PrintDisplayInfo(gpuInfo);
     printf(
         "-----------------------------------------------------------------\n");
-
-    if (0) {
-      printf("\n");
-      testDriver(gpuInfo.radeonSoftwareVersion, AGS_MAKE_VERSION(20, 1, 0));
-      testDriver("18.8.randombetadriver", AGS_MAKE_VERSION(18, 8, 2));
-      testDriver("18.8.123randomdriver", AGS_MAKE_VERSION(18, 8, 2));
-      testDriver("18.9.randomdriver", AGS_MAKE_VERSION(18, 8, 2));
-      testDriver("18.8.2", AGS_MAKE_VERSION(18, 8, 2));
-      testDriver("18.8.2", AGS_MAKE_VERSION(18, 8, 1));
-      testDriver("18.8.2", AGS_MAKE_VERSION(18, 8, 3));
-      printf("\n");
-    }
 
     if (agsDeInitialize(agsContext) != AGS_SUCCESS) {
       printf("Failed to cleanup AGS Library\n");
@@ -271,11 +245,11 @@ int GetGpuUsage() {
 #if defined(LINUX)
   hDLL = dlopen("libatiadlxx.so", RTLD_LAZY | RTLD_GLOBAL);
 #else
-  hDLL = LoadLibrary("atiadlxx.dll");
+  hDLL = LoadLibrary(L"atiadlxx.dll");
   if (hDLL == NULL)
     // A 32 bit calling application on 64 bit OS will fail to LoadLIbrary.
     // Try to load the 32 bit library (atiadlxy.dll) instead
-    hDLL = LoadLibrary("atiadlxy.dll");
+    hDLL = LoadLibrary(L"atiadlxy.dll");
 #endif
 
   if (NULL == hDLL) {
