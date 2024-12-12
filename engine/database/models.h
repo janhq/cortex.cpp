f473b0b2d78074d4ebb2e61540de470b62740ea1
vansangpfiev <vansangpfiev@gmail.com>
2024-12-12 11:29:11 +0700

    feat: model sources (#1777)
    * feat: prioritize GPUs
    
    * fix: migrate db
    
    * fix: add priority
    
    * fix: db
    
    * fix: more
    
    * feat: model sources
    
    * feat: support delete API
    
    * feat: cli: support models sources add
    
    * feat: cli: model source delete
    
    * feat: cli: add model source list
    
    * feat: sync cortex.db
    
    * chore: cleanup
    
    * feat: add metadata for model
    
    * fix: migration
    
    * chore: unit tests: cleanup
    
    * fix: add metadata
    
    * fix: pull model
    
    * chore: unit tests: update
    
    * chore: add e2e tests for models sources
    
    * chore: add API docs
    
    * chore: rename
    
    ---------
    
    Co-authored-by: vansangpfiev <sang@jan.ai>

--- engine/database/models.h
+++ engine/database/models.h
@@ -8,7 +8,8 @@
 
 namespace cortex::db {
 
-enum class ModelStatus { Remote, Downloaded, Undownloaded };
+enum class ModelStatus { Remote, Downloaded, Downloadable };
+
 
 struct ModelEntry {
   std::string model;
@@ -20,6 +21,7 @@ struct ModelEntry {
   std::string model_source;
   ModelStatus status;
   std::string engine;
+  std::string metadata;
 };
 
 class Models {
@@ -28,8 +30,7 @@ class Models {
   SQLite::Database& db_;
 
   bool IsUnique(const std::vector<ModelEntry>& entries,
-                const std::string& model_id,
-                const std::string& model_alias) const;
+                const std::string& model_id) const;
 
   cpp::result<std::vector<ModelEntry>, std::string> LoadModelListNoLock() const;
 
@@ -41,23 +42,24 @@ class Models {
   Models();
   Models(SQLite::Database& db);
   ~Models();
-  std::string GenerateShortenedAlias(
-      const std::string& model_id,
-      const std::vector<ModelEntry>& entries) const;
   cpp::result<ModelEntry, std::string> GetModelInfo(
       const std::string& identifier) const;
   void PrintModelInfo(const ModelEntry& entry) const;
-  cpp::result<bool, std::string> AddModelEntry(ModelEntry new_entry,
-                                               bool use_short_alias = false);
+  cpp::result<bool, std::string> AddModelEntry(ModelEntry new_entry);
   cpp::result<bool, std::string> UpdateModelEntry(
       const std::string& identifier, const ModelEntry& updated_entry);
   cpp::result<bool, std::string> DeleteModelEntry(
       const std::string& identifier);
-  cpp::result<bool, std::string> UpdateModelAlias(
-      const std::string& model_id, const std::string& model_alias);
+  cpp::result<bool, std::string> DeleteModelEntryWithOrg(
+      const std::string& src);
+  cpp::result<bool, std::string> DeleteModelEntryWithRepo(
+      const std::string& src);
   cpp::result<std::vector<std::string>, std::string> FindRelatedModel(
       const std::string& identifier) const;
   bool HasModel(const std::string& identifier) const;
+  cpp::result<std::vector<std::string>, std::string> GetModelSources() const;
+  cpp::result<std::vector<std::string>, std::string> GetModels(
+      const std::string& model_src) const;
 };
 
 }  // namespace cortex::db
