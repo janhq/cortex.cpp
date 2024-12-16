e3396868b79fd4427d44a5eab82e50c864aa1d22
James <namnh0122@gmail.com>
2024-12-16 10:08:01 +0700

    feat: add openai assistant

---
+++ engine/common/assistant_code_interpreter_tool.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "common/assistant_tool.h"
+
+namespace OpenAi {
+struct AssistantCodeInterpreterTool : public AssistantTool {
+  AssistantCodeInterpreterTool() : AssistantTool("code_interpreter") {}
+
+  AssistantCodeInterpreterTool(const AssistantCodeInterpreterTool&) = delete;
+
+  AssistantCodeInterpreterTool& operator=(const AssistantCodeInterpreterTool&) =
+      delete;
+
+  AssistantCodeInterpreterTool(AssistantCodeInterpreterTool&&) = default;
+
+  AssistantCodeInterpreterTool& operator=(AssistantCodeInterpreterTool&&) =
+      default;
+
+  ~AssistantCodeInterpreterTool() = default;
+
+  static cpp::result<AssistantCodeInterpreterTool, std::string> FromJson() {
+    AssistantCodeInterpreterTool tool;
+    return std::move(tool);
+  }
+
+  cpp::result<Json::Value, std::string> ToJson() override {
+    Json::Value json;
+    json["type"] = type;
+    return json;
+  }
+};
+}  // namespace OpenAi
--- engine/services/assistant_service.h
+++ engine/services/assistant_service.h
@@ -1,15 +1,14 @@
 #pragma once
 
 #include "common/assistant.h"
+#include "common/dto/assistant_create_dto.h"
+#include "common/dto/assistant_update_dto.h"
+#include "common/repository/assistant_repository.h"
 #include "repositories/thread_fs_repository.h"
 #include "utils/result.hpp"
 
 class AssistantService {
  public:
-  explicit AssistantService(
-      std::shared_ptr<AssistantBackwardCompatibleSupport> thread_repository)
-      : thread_repository_{thread_repository} {}
-
   cpp::result<OpenAi::JanAssistant, std::string> CreateAssistant(
       const std::string& thread_id, const OpenAi::JanAssistant& assistant);
 
@@ -19,6 +18,31 @@ class AssistantService {
   cpp::result<OpenAi::JanAssistant, std::string> ModifyAssistant(
       const std::string& thread_id, const OpenAi::JanAssistant& assistant);
 
+  // V2
+  cpp::result<OpenAi::Assistant, std::string> CreateAssistantV2(
+      const dto::CreateAssistantDto& create_dto);
+
+  cpp::result<std::vector<OpenAi::Assistant>, std::string> ListAssistants(
+      uint8_t limit, const std::string& order, const std::string& after,
+      const std::string& before) const;
+
+  cpp::result<OpenAi::Assistant, std::string> RetrieveAssistantV2(
+      const std::string& assistant_id) const;
+
+  cpp::result<OpenAi::Assistant, std::string> ModifyAssistantV2(
+      const std::string& assistant_id,
+      const dto::UpdateAssistantDto& update_dto);
+
+  cpp::result<void, std::string> DeleteAssistantV2(
+      const std::string& assistant_id);
+
+  explicit AssistantService(
+      std::shared_ptr<AssistantBackwardCompatibleSupport> thread_repository,
+      std::shared_ptr<AssistantRepository> assistant_repository)
+      : thread_repository_{thread_repository},
+        assistant_repository_{assistant_repository} {}
+
  private:
   std::shared_ptr<AssistantBackwardCompatibleSupport> thread_repository_;
+  std::shared_ptr<AssistantRepository> assistant_repository_;
 };
