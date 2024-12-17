6ccd2cc680c4ff9906aa779df1a9c20736f50879
James <namnh0122@gmail.com>
2024-12-17 13:10:01 +0700

    fix: swagger getting configuration from config file

--- engine/controllers/swagger.h
+++ engine/controllers/swagger.h
@@ -5,12 +5,37 @@
 
 using namespace drogon;
 
-class SwaggerController : public drogon::HttpController<SwaggerController> {
+class SwaggerController
+    : public drogon::HttpController<SwaggerController, false> {
+
+  constexpr static auto ScalarUi = R"(
+<!doctype html>
+<html>
+  <head>
+    <title>Cortex API Reference</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1" />
+  </head>
+  <body>
+    <!-- Need a Custom Header? Check out this example https://codepen.io/scalarorg/pen/VwOXqam -->
+    <script
+      id="api-reference"
+      data-url="/openapi.json"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+  </body>
+</html>
+)";
+
  public:
   METHOD_LIST_BEGIN
   ADD_METHOD_TO(SwaggerController::serveSwaggerUI, "/", Get);
   ADD_METHOD_TO(SwaggerController::serveOpenAPISpec, "/openapi.json", Get);
   METHOD_LIST_END
+
+  explicit SwaggerController(const std::string& host, const std::string& port)
+      : host_{host}, port_{port} {};
 
   void serveSwaggerUI(
       const drogon::HttpRequestPtr& req,
@@ -21,6 +46,8 @@ class SwaggerController : public drogon::HttpController<SwaggerController> {
       std::function<void(const drogon::HttpResponsePtr&)>&& callback) const;
 
  private:
-  static const std::string swaggerUIHTML;
-  static Json::Value generateOpenAPISpec();
+  std::string host_;
+  std::string port_;
+
+  Json::Value GenerateOpenApiSpec() const;
 };
