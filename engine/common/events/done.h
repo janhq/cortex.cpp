1ee334ba75d05a6f5892a85e6961be0c43575013
James <namnh0122@gmail.com>
2025-01-03 08:53:22 +0700

    update

--- engine/common/events/done.h
+++ engine/common/events/done.h
@@ -1,11 +1,19 @@
 #pragma once
 
+#include <json/value.h>
 #include "common/events/assistant_stream_event.h"
 
 namespace OpenAi {
-struct ErrorEvent : public AssistantStreamEvent {
-  std::string event{"done"};
+struct DoneEvent : public AssistantStreamEvent {
+  DoneEvent() : AssistantStreamEvent{"done"} {}
+
+  ~DoneEvent() = default;
 
   std::string data{"[DONE]"};
+
+  auto SingleLineJsonData() const
+      -> cpp::result<std::string, std::string> override {
+    return data;
+  }
 };
 }  // namespace OpenAi
--- engine/common/run_step_delta.h
+++ engine/common/run_step_delta.h
@@ -1,29 +1,9 @@
 #pragma once
 
 #include <string>
+#include "common/run_step_detail.h"
 
 namespace OpenAi {
-struct RunStepDetails {
-  virtual ~RunStepDetails() = default;
-
-  std::string type;
-};
-
-struct MessageCreationDetail : public RunStepDetails {
-  struct MessageCreation {
-    std::string message_id;
-  };
-
-  std::string type{"message_creation"};
-
-  MessageCreation message_creation;
-};
-
-struct ToolCalls : public RunStepDetails {
-  std::string type{"tool_calls"};
-  // TODO: namh implement toolcalls later
-};
-
 struct RunStepDelta {
   struct Delta {
     /**
