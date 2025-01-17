763026e290bc12d2560983f2988538c76a77a967
vansangpfiev <vansangpfiev@gmail.com>
2025-01-17 11:59:46 +0700

    fix: add cpu usage (#1868)
    Co-authored-by: vansangpfiev <sang@jan.ai>

--- engine/utils/hardware/cpu_info.h
+++ engine/utils/hardware/cpu_info.h
@@ -5,6 +5,7 @@
 #include <string_view>
 #include <vector>
 #include "common/hardware_common.h"
+#include "cpu_usage.h"
 #include "hwinfo/hwinfo.h"
 #include "utils/cpuid/cpu_info.h"
 
@@ -15,9 +16,12 @@ inline CPU GetCPUInfo() {
     return CPU{};
   auto cpu = res[0];
   cortex::cpuid::CpuInfo inst;
+  float usage = GetCPUUsage();
+  // float usage = 0;
   return CPU{.cores = cpu.numPhysicalCores(),
              .arch = std::string(GetArch()),
              .model = cpu.modelName(),
+             .usage = usage,
              .instructions = inst.instructions()};
 }
 }  // namespace cortex::hw
