e48a0b4699a77646f0cfc02670528e965d042e9a
vansangpfiev <vansangpfiev@gmail.com>
2025-02-10 11:55:32 +0700

    fix: correct gpu info list (#1944)
    * fix: correct gpu info list
    
    * chore: cleanup
    
    ---------
    
    Co-authored-by: vansangpfiev <sang@jan.ai>

--- engine/utils/hardware/gpu_info.h
+++ engine/utils/hardware/gpu_info.h
@@ -8,16 +8,15 @@
 namespace cortex::hw {
 
 inline std::vector<GPU> GetGPUInfo() {
-  std::vector<GPU> res;
-  // Only support for nvidia for now
-  // auto gpus = hwinfo::getAllGPUs();
   auto nvidia_gpus = system_info_utils::GetGpuInfoList();
   auto vulkan_gpus = GetGpuInfoList().value_or(std::vector<cortex::hw::GPU>{});
-  // add more information for GPUs
+  auto use_vulkan_info = nvidia_gpus.empty();
 
+  // In case we have vulkan info, add more information for GPUs
   for (size_t i = 0; i < nvidia_gpus.size(); i++) {
     for (size_t j = 0; j < vulkan_gpus.size(); j++) {
       if (nvidia_gpus[i].uuid.find(vulkan_gpus[j].uuid) != std::string::npos) {
+        use_vulkan_info = true;
         vulkan_gpus[j].version =
             nvidia_gpus[0].cuda_driver_version.value_or("unknown");
         vulkan_gpus[j].add_info = NvidiaAddInfo{
@@ -28,6 +27,25 @@ inline std::vector<GPU> GetGPUInfo() {
       }
     }
   }
-  return vulkan_gpus;
+  
+  if (use_vulkan_info) {
+    return vulkan_gpus;
+  } else {
+    std::vector<GPU> res;
+    for (auto& n : nvidia_gpus) {
+      res.emplace_back(
+          GPU{.id = n.id,
+              .name = n.name,
+              .version = nvidia_gpus[0].cuda_driver_version.value_or("unknown"),
+              .add_info =
+                  NvidiaAddInfo{
+                      .driver_version = n.driver_version.value_or("unknown"),
+                      .compute_cap = n.compute_cap.value_or("unknown")},
+              .free_vram = std::stoi(n.vram_free),
+              .total_vram = std::stoi(n.vram_total),
+              .uuid = n.uuid});
+    }
+    return res;
+  }
 }
 }  // namespace cortex::hw
