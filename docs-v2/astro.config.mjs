5399557963f01d9b4fbcaf4f5efe714ea487f866
Ramon Perez <ramonpzg@protonmail.com>
2025-02-21 16:27:37 +1100

    working on new docs site

---
+++ docs-v2/astro.config.mjs
@@ -0,0 +1,51 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Cortex",
+      social: {
+        github: "https://github.com/withastro/starlight",
+        "x.com": "https://example.com",
+        discord: "https://example.com",
+      },
+      sidebar: [
+        {
+          label: "Getting Started",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Capabilities",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "How-To Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Tutorials",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
