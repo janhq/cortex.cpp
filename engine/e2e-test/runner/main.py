8a20ce739d042d0d849bf65c1ca89ad418e853fa
Harry Le <harry@menlo.ai>
2025-02-22 20:29:33 +0700

    test: update path

--- engine/e2e-test/runner/main.py
+++ engine/e2e-test/runner/main.py
@@ -16,21 +16,21 @@ sys.path.append(os.path.join(PROJECT_ROOT, "cli/model"))
 sys.path.append(os.path.join(PROJECT_ROOT, "cli/common"))  
 
 ### e2e tests are expensive, have to keep engines tests in order
-from test_api_get_list_engine import TestApiEngineList
-from test_api_engine import TestApiEngine
-from test_api_model import TestApiModel
-from test_api_model_import import TestApiModelImport
+from api.engines.test_api_get_list_engine import TestApiEngineList
+from api.engines.test_api_engine import TestApiEngine
+from api.model.test_api_model import TestApiModel
+from api.model.test_api_model_import import TestApiModelImport
 
 ###
-from test_cli_engine_get import TestCliEngineGet
-from test_cli_engine_install import TestCliEngineInstall
-from test_cli_engine_list import TestCliEngineList
-from test_cli_engine_uninstall import TestCliEngineUninstall
-from test_cli_model import TestCliModel
-from test_cli_model_import import TestCliModelImport
-from test_cli_server_start import TestCliServerStart
-from test_cortex_update import TestCortexUpdate
-from test_create_log_folder import TestCreateLogFolder
+from cli.engines.test_cli_engine_get import TestCliEngineGet
+from cli.engines.test_cli_engine_install import TestCliEngineInstall
+from cli.engines.test_cli_engine_list import TestCliEngineList
+from cli.engines.test_cli_engine_uninstall import TestCliEngineUninstall
+from cli.model.test_cli_model import TestCliModel
+from cli.model.test_cli_model_import import TestCliModelImport
+from cli.common.test_cli_server_start import TestCliServerStart
+from cli.common.test_cortex_update import TestCortexUpdate
+from cli.common.test_create_log_folder import TestCreateLogFolder
 
 if __name__ == "__main__":
     sys.exit(pytest.main([__file__, "-v"]))
