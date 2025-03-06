e5a34b83a0749703da30b47a1e3c5ff6744cadc8
Harry Le <harry@menlo.ai>
2025-03-06 11:22:31 +0700

    test: debug

--- engine/e2e-test/api/files/test_api_create_file.py
+++ engine/e2e-test/api/files/test_api_create_file.py
@@ -24,22 +24,28 @@ class TestApiCreateFile:
         
     def test_api_create_file_successfully(self):
         # Define file path
-        file_path = os.path.join("e2e-test", "api", "files", "blank.txt")
+        file_path_rel = os.path.join("e2e-test", "api", "files", "blank.txt")
+        file_path = os.path.join(os.getcwd(), file_path_rel)
+
         log_response(file_path, "test_api_create_file_successfully")
 
 
-        # Prepare request data
-        files = {
-            "file": ("blank.txt", open(file_path, "rb"), "text/plain")
-        }
-        data = {
-            "purpose": "assistants"
-        }
+        # # Prepare request data
+        # files = {
+        #     "file": ("blank.txt", open(file_path, "rb"), "text/plain")
+        # }
+        # data = {
+        #     "purpose": "assistants"
+        # }
 
         post_file_url = "http://127.0.0.1:3928/v1/files"
-        response = requests.post(post_file_url, files=files, data=data)
-        log_response(response.text, "test_api_create_file_successfully")
-        log_response(response.status_code, "test_api_create_file_successfully")
+        # response = requests.post(post_file_url, files=files, data=data)
+        with open(file_path, "rb") as file:
+            files = {"file": ("blank.txt", file, "text/plain")}
+            data = {"purpose": "assistants"}
+            response = requests.post(post_file_url, files=files, data=data)
+            log_response(response.text, "test_api_create_file_successfully")
+            log_response(response.status_code, "test_api_create_file_successfully")
 
         json_data = response.json()
         log_response(json_data, "test_api_create_file_successfully")
