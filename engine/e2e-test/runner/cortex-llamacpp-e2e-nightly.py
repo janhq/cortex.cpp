8681544da2515ddb6fdd786e00452141a57c6e37
Le Vinh <43307514+LeVinhGithub@users.noreply.github.com>
2025-03-07 09:30:36 +0700

    task: Expand e2e test for section "files" #2065
    * test: add e2e files
    
    
    ---------
    
    Co-authored-by: Harry Le <harry@menlo.ai>

--- engine/e2e-test/runner/cortex-llamacpp-e2e-nightly.py
+++ engine/e2e-test/runner/cortex-llamacpp-e2e-nightly.py
@@ -24,6 +24,10 @@ from api.engines.test_api_get_engine_release_latest import TestApiEngineReleaseL
 from test_api_post_default_engine import TestApiSetDefaultEngine
 from api.model.test_api_model import TestApiModel
 from api.model.test_api_model_import import TestApiModelImport
+from api.files.test_api_create_file import TestApiCreateFile
+from api.files.test_api_get_file import TestApiGetFile
+from api.files.test_api_get_list_file import TestApiGetListFile
+from api.files.test_api_delete_file import TestApiDeleteFile
 from api.message.test_api_get_message import TestApiGetMessage
 from api.message.test_api_get_list_message import TestApiGetListMessage
 from api.message.test_api_create_message import TestApiCreateMessage
