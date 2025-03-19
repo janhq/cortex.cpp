0f2fa6e988f724c2b9b1b99ab826a12ca4b55fca
sangjanai <sang@jan.ai>
2025-03-19 11:07:42 +0700

    chore: cleanup

--- engine/utils/engine_constants.h
+++ engine/utils/engine_constants.h
@@ -5,9 +5,12 @@ constexpr const auto kLlamaEngine = "llama-cpp";
 constexpr const auto kRemote = "remote";
 constexpr const auto kLocal = "local";
 
-
 constexpr const auto kLlamaRepo = "llama.cpp";
-constexpr const auto kLlamaLibPath = "./engines/cortex.llamacpp";
+constexpr const auto kLlamaLibPath = "./engines/llama.cpp";
+constexpr const auto kLlamaServer = "llama-server";
+
+constexpr const auto kMenloOrg = "menloresearch";
+constexpr const auto kGgmlOrg = "ggml-org";
 
 // other constants
 constexpr auto static kHuggingFaceHost = "huggingface.co";
@@ -18,6 +21,7 @@ constexpr auto static kDefaultGHUserAgent = "cortexcpp";
 constexpr auto static kWindowsOs = "win";
 constexpr auto static kMacOs = "mac";
 constexpr auto static kLinuxOs = "linux";
+constexpr auto static kUbuntuOs = "ubuntu";
 constexpr auto static kUnsupportedOs = "Unsupported OS";
 
 constexpr auto static kCurlGetTimeout = 10;
