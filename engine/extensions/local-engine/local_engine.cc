fa6b437f8c04f0fe70ddeeb338a15fb7abb62cd6
Akarshan <akarshan@menlo.ai>
2025-06-14 10:37:32 +0530

    feat: add --no-webui flag to model params vector

--- engine/extensions/local-engine/local_engine.cc
+++ engine/extensions/local-engine/local_engine.cc
@@ -51,6 +51,8 @@ int GenerateRandomInteger(int min, int max) {
 
 std::vector<std::string> ConvertJsonToParamsVector(const Json::Value& root) {
   std::vector<std::string> res;
+  std::string errors;
+  res.push_back("--no-webui");
 
   for (const auto& member : root.getMemberNames()) {
     if (member == "model_path" || member == "llama_model_path") {
